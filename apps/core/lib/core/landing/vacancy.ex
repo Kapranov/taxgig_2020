9cd1d422385259d8152f58293cd8a2d7e0c31cc7
Oleg G.Kapranov <ok@pure-agency.co>
2020-01-22 01:49:04 -1000

    added schema Vacancy

--- apps/core/lib/core/landing/vacancy.ex
+++ apps/core/lib/core/landing/vacancy.ex
@@ -2,4 +2,35 @@ defmodule Core.Landing.Vacancy do
   @moduledoc """
   Schema for Vacancies.
   """
+
+  use Core.Model
+
+  @allowed_params ~w(
+    title
+    content
+    department
+  )
+
+  @required_params ~w(
+    title
+    content
+    department
+  )
+
+  schema "vacancies" do
+    field :title, :string
+    field :content, :string
+    field :department, :string
+
+    timestamps()
+  end
+
+  @doc """
+  Create changeset for Vacancy.
+  """
+  def changeset(struct, attrs) do
+    struct
+    |> cast(attrs, @allowed_params)
+    |> validate_required(@required_params)
+  end
 end
