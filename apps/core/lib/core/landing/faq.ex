b5c9e8d635f6c4a50e3c649ac0ba9c9bf4e03bef
Oleg G.Kapranov <ok@pure-agency.co>
2020-01-22 01:43:57 -1000

    added schema Faq

--- apps/core/lib/core/landing/faq.ex
+++ apps/core/lib/core/landing/faq.ex
@@ -2,4 +2,36 @@ defmodule Core.Landing.Faq do
   @moduledoc """
   Schema for Faqs.
   """
+
+  use Core.Model
+
+  alias Core.Landing.FaqCategory
+
+  @allowed_params ~w(
+    title
+    content
+  )
+
+  @required_params ~w(
+    title
+    content
+  )
+
+  schema "faqs" do
+    field :title, :string
+    field :content, :string
+
+    belongs_to :faq_category, FaqCategory
+
+    timestamps()
+  end
+
+  @doc """
+  Create changeset for Faq.
+  """
+  def changeset(struct, attrs) do
+    struct
+    |> cast(attrs, @allowed_params)
+    |> validate_required(@required_params)
+  end
 end
