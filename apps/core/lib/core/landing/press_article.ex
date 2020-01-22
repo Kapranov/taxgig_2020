8b0d4f41718d892d9989350b3a39f74cf71ac123
Oleg G.Kapranov <ok@pure-agency.co>
2020-01-22 01:46:37 -1000

    added schema PressArticle

--- apps/core/lib/core/landing/press_article.ex
+++ apps/core/lib/core/landing/press_article.ex
@@ -2,4 +2,38 @@ defmodule Core.Landing.PressArticle do
   @moduledoc """
   Schema for PressArticles.
   """
+
+  use Core.Model
+
+  @allowed_params ~w(
+    title
+    author
+    preview_text
+    url
+  )
+
+  @required_params ~w(
+    title
+    author
+    preview_text
+    url
+  )
+
+  schema "faqs" do
+    field :title, :string
+    field :author, :string
+    field :preview_text, :string
+    field :url, :string
+
+    timestamps()
+  end
+
+  @doc """
+  Create changeset for PressArticle.
+  """
+  def changeset(struct, attrs) do
+    struct
+    |> cast(attrs, @allowed_params)
+    |> validate_required(@required_params)
+  end
 end
