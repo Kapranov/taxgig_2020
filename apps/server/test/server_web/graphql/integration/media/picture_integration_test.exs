91d9c9246147cd78d84930ee1928ce26797b9bc4
Oleg G.Kapranov <ok@pure-agency.co>
2020-05-11 07:57:31 -1000

    added updatedPicture tests

--- apps/server/test/server_web/graphql/integration/media/picture_integration_test.exs
+++ apps/server/test/server_web/graphql/integration/media/picture_integration_test.exs
@@ -305,18 +305,368 @@ defmodule ServerWeb.GraphQL.Integration.Media.PictureIntegrationTest do
     end
 
     it "update specific picture by id - `Absinthe.run`" do
+      struct = insert(:picture)
+      user = Core.Accounts.User.find_by(id: struct.profile_id)
+      public_endpoint = Application.get_env(:core, Core.Uploaders.S3)[:public_endpoint]
+      context = %{current_user: user}
+      picture = %{name: "Time for #NeverBernie", alt: "I woke up this morning wondering whether it’s time to unfurl the #NeverBernie banner.", file: "bernie.jpg"}
+
+      query = """
+      mutation f(
+          $profile: String!,
+          $input: PictureInputObject
+        ) {
+        updatePicture(
+          profileId: $profile,
+          file: {
+            picture: $input
+          }
+        ) {
+          id
+          content_type
+          name
+          size
+          url
+        }
+      }
+      """
+
+      variables = %{
+        picture => %Plug.Upload{
+          content_type: "image/jpg",
+          filename: picture.file,
+          path: @bernie_path
+        },
+        "profile" => struct.profile_id,
+        "PictureInputObject" => %{
+          "alt" => picture.alt,
+          "file" => picture.file,
+          "name" => picture.name
+        },
+      }
+
+      {:ok, %{data: %{"updatePicture" => updated}}} =
+        Absinthe.run(query, Schema, [context: context, operation_name: "f", variables: variables])
+
+      assert updated["id"]           == struct.id
+      assert updated["content_type"] == "image/jpg"
+      assert updated["name"]         == "Logo"
+      assert updated["size"]         == 5024
+      assert updated["url"]          =~ public_endpoint
+      assert {:ok, %{
+          body: "",
+          headers: [
+            {"x-amz-request-id", _x_amz_request_id},
+            {"Date", _time_remove_file},
+            {"Strict-Transport-Security", "max-age=15552000; includeSubDomains; preload"}
+          ],
+          status_code: 204
+        }
+      } = Core.Upload.remove(updated["url"])
     end
 
     it "nothing change for missing params - `AbsintheHelpers`" do
+      struct = insert(:picture)
+      user = Core.Accounts.User.find_by(id: struct.profile_id)
+
+      query = """
+      {
+        updatePicture(
+          profileId: \"#{struct.profile_id}\"
+        ) {
+          id
+          content_type
+          name
+          size
+          url
+        }
+      }
+      """
+
+      map = %{
+        "query" => "mutation #{query}"
+      }
+
+      res =
+        build_conn()
+        |> auth_conn(user)
+        |> put_req_header("content-type", "multipart/form-data")
+        |> post("/graphiql", map)
+
+      assert json_response(res, 200)["errors"] == nil
+
+      updated = json_response(res, 200)["data"]["updatePicture"]
+
+      assert updated["id"]           == struct.id
+      assert updated["content_type"] == struct.file.content_type
+      assert updated["name"]         == struct.file.name
+      assert updated["size"]         == struct.file.size
+      assert updated["url"]          == struct.file.url
+      assert {:ok, %{
+          body: "",
+          headers: [
+            {"x-amz-request-id", _x_amz_request_id},
+            {"Date", _time_remove_file},
+            {"Strict-Transport-Security", "max-age=15552000; includeSubDomains; preload"}
+          ],
+          status_code: 204
+        }
+      } = Core.Upload.remove(updated["url"])
     end
 
     it "nothing change for missing params - `Absinthe.run`" do
+      struct = insert(:picture)
+      user = Core.Accounts.User.find_by(id: struct.profile_id)
+      context = %{current_user: user}
+
+      query = """
+      mutation f(
+          $profile: String!
+        ) {
+        updatePicture(
+          profileId: $profile
+        ) {
+          id
+          content_type
+          name
+          size
+          url
+        }
+      }
+      """
+
+      variables = %{"profile" => struct.profile_id}
+
+      {:ok, %{data: %{"updatePicture" => updated}}} =
+        Absinthe.run(query, Schema, [context: context, operation_name: "f", variables: variables])
+
+      assert updated["id"]           == struct.id
+      assert updated["content_type"] == struct.file.content_type
+      assert updated["name"]         == struct.file.name
+      assert updated["size"]         == struct.file.size
+      assert updated["url"]          =~ struct.file.url
+      assert {:ok, %{
+          body: "",
+          headers: [
+            {"x-amz-request-id", _x_amz_request_id},
+            {"Date", _time_remove_file},
+            {"Strict-Transport-Security", "max-age=15552000; includeSubDomains; preload"}
+          ],
+          status_code: 204
+        }
+      } = Core.Upload.remove(updated["url"])
     end
 
     it "returns error for missing params - `AbsintheHelpers`" do
+      struct = insert(:picture)
+      user = Core.Accounts.User.find_by(id: struct.profile_id)
+
+      query = """
+      mutation {
+        updatePicture(
+          profileId: nil,
+          file: {
+            picture: {
+              alt: nil,
+              file: nil,
+              name: nil
+            }
+          }
+        ) {
+          id
+          content_type
+          name
+          size
+          url
+        }
+      }
+      """
+
+      res =
+        build_conn()
+        |> auth_conn(user)
+        |> post("/graphiql", AbsintheHelpers.mutation_skeleton(query))
+
+      assert json_response(res, 200)["errors"] == [
+        %{"locations" => [%{
+              "column" => 0,
+              "line" => 3
+            }
+          ],
+          "message" => "Argument \"profileId\" has invalid value nil."
+        },
+        %{
+          "locations" => [%{
+              "column" => 0,
+              "line" => 4
+            }
+          ],
+          "message" => "Argument \"file\" has invalid value {picture: {alt: nil, file: nil, name: nil}}.\nIn field \"picture\": Expected type \"PictureInputObject\", found {alt: nil, file: nil, name: nil}.\nIn field \"alt\": Expected type \"String\", found nil.\nIn field \"file\": Expected type \"Upload!\", found nil.\nIn field \"name\": Expected type \"String!\", found nil."
+        }]
+      assert {:ok, %{
+          body: "",
+          headers: [
+            {"x-amz-request-id", _x_amz_request_id},
+            {"Date", _time_remove_file},
+            {"Strict-Transport-Security", "max-age=15552000; includeSubDomains; preload"}
+          ],
+          status_code: 204
+        }
+      } = Core.Upload.remove(struct.file.url)
     end
 
     it "returns error for missing params - `Absinthe.run`" do
+      struct = insert(:picture)
+      user = Core.Accounts.User.find_by(id: struct.profile_id)
+      context = %{current_user: user}
+
+      query = """
+      mutation {
+        updatePicture(
+          profileId: nil,
+          file: {
+            picture: nil
+          }
+        ) {
+          id
+          content_type
+          name
+          size
+          url
+        }
+      }
+      """
+
+      variables = %{}
+
+      {:ok, %{errors: [%{
+              locations: [%{column: 0, line: 3}],
+              message: "Argument \"profileId\" has invalid value nil."
+            }]
+        }} = Absinthe.run(query, Schema, [context: context, variables: variables])
+
+      assert {:ok, %{
+          body: "",
+          headers: [
+            {"x-amz-request-id", _x_amz_request_id},
+            {"Date", _time_remove_file},
+            {"Strict-Transport-Security", "max-age=15552000; includeSubDomains; preload"}
+          ],
+          status_code: 204
+        }
+      } = Core.Upload.remove(struct.file.url)
+
+    end
+
+    test "update_picture/3 forbids uploading if no auth - AbsintheHelpers" do
+      struct = insert(:picture)
+      user = insert(:tp_user)
+      picture = %{name: "Time for #NeverBernie", alt: "I woke up this morning wondering whether it’s time to unfurl the #NeverBernie banner.", file: "bernie.jpg"}
+
+      query = """
+      {
+        updatePicture(
+          profileId: \"#{struct.profile_id}\",
+          file: {
+            picture: {
+              alt: \"#{picture.alt}\",
+              file: \"#{picture.file}\",
+              name: \"#{picture.name}\"
+            }
+          }
+        ) {
+          id
+          content_type
+          name
+          size
+          url
+        }
+      }
+      """
+
+      map = %{
+        "query" => "mutation #{query}",
+        picture.file => %Plug.Upload{
+          filename: picture.file,
+          path: @bernie_path
+        }
+      }
+
+      res =
+        build_conn()
+        |> auth_conn(user)
+        |> put_req_header("content-type", "multipart/form-data")
+        |> post("/graphiql", map)
+
+      assert hd(json_response(res, 200)["errors"])["message"] == "permission denied"
+
+      assert {:ok, %{
+          body: "",
+          headers: [
+            {"x-amz-request-id", _x_amz_request_id},
+            {"Date", _time_remove_file},
+            {"Strict-Transport-Security", "max-age=15552000; includeSubDomains; preload"}
+          ],
+          status_code: 204
+        }
+      } = Core.Upload.remove(struct.file.url)
+    end
+
+    test "update_picture/3 forbids uploading if no auth - `Absinthe.run`" do
+      struct = insert(:picture)
+      user = insert(:tp_user)
+      context = %{current_user: user}
+      picture = %{name: "Time for #NeverBernie", alt: "I woke up this morning wondering whether it’s time to unfurl the #NeverBernie banner.", file: "bernie.jpg"}
+
+      query = """
+      mutation f(
+          $profile: String!,
+          $input: PictureInputObject
+        ) {
+        updatePicture(
+          profileId: $profile,
+          file: {
+            picture: $input
+          }
+        ) {
+          id
+          content_type
+          name
+          size
+          url
+        }
+      }
+      """
+
+      variables = %{
+        picture => %Plug.Upload{
+          content_type: "image/jpg",
+          filename: picture.file,
+          path: @bernie_path
+        },
+        "profile" => struct.profile_id,
+        "PictureInputObject" => %{
+          "alt" => picture.alt,
+          "file" => picture.file,
+          "name" => picture.name
+        },
+      }
+
+      {:ok, %{data: %{"updatePicture" => updated}}} =
+        Absinthe.run(query, Schema, [context: context, operation_name: "f", variables: variables])
+
+      assert updated == nil
+
+      assert {:ok, %{
+          body: "",
+          headers: [
+            {"x-amz-request-id", _x_amz_request_id},
+            {"Date", _time_remove_file},
+            {"Strict-Transport-Security", "max-age=15552000; includeSubDomains; preload"}
+          ],
+          status_code: 204
+        }
+      } = Core.Upload.remove(struct.file.url)
     end
   end
 
