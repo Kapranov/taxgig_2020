cf251c972b0fe6dbbea3afdba3c46a38f54b7f31
Oleg G.Kapranov <ok@pure-agency.co>
2020-05-31 22:31:52 -1000

    fixed Chat Logger.info

--- apps/chat/lib/chat.ex
+++ apps/chat/lib/chat.ex
@@ -5,8 +5,13 @@ defmodule Chat do
   require Logger
 
   def start(_type, _args) do
-    [ip: _ip, scheme: schema, host: host, port: _port] = Application.get_env(:server, ServerWeb.Endpoint)[:url]
-    Logger.info("Running #{inspect(__MODULE__)} with Cowboy using #{schema}://#{host}:#{port()}")
+    if Mix.env == :test do
+      [host: host, port: _port, ip: _ip] = Application.get_env(:server, ServerWeb.Endpoint)[:url]
+      Logger.info("Running #{inspect(__MODULE__)} with Cowboy using http://#{host}:#{port()}")
+    else
+      [ip: _ip, scheme: schema, host: host, port: _port] = Application.get_env(:server, ServerWeb.Endpoint)[:url]
+      Logger.info("Running #{inspect(__MODULE__)} with Cowboy using #{schema}://#{host}:#{port()}")
+    end
     Chat.Application.start_link([])
   end
 
