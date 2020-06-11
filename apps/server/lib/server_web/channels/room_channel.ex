08826c657f626b102c672b8371bdc477b892e63b
Oleg G.Kapranov <ok@pure-agency.co>
2020-06-11 01:25:02 -1000

    updated ServerWeb.RoomChannel

--- apps/server/lib/server_web/channels/room_channel.ex
+++ apps/server/lib/server_web/channels/room_channel.ex
@@ -11,7 +11,6 @@ defmodule ServerWeb.RoomChannel do
   }
 
   alias ServerWeb.{
-    Endpoint,
     ErrorView,
     RoomView
   }
@@ -70,31 +69,73 @@ defmodule ServerWeb.RoomChannel do
     end
   end
 
-  def handle_in("update", _payload, _socket) do
+  def handle_in("update", payload, socket) do
+    params = %{
+      name:               payload["name"],
+      description: payload["description"],
+      topic:             payload["topic"],
+      user_id:         payload["user_id"]
+    }
+
+    cond do
+      is_nil(payload["id"]) ->
+        {:reply, {:error, ErrorView.render("404.json")}, socket}
+      true ->
+        try do
+          room = Repo.get!(Room, payload["id"])
+          case Repo.update(Room.changeset(room, params)) do
+            {:ok, struct} ->
+              response = RoomView.render("show.json", %{rooms: struct})
+              {:reply, {:ok, response}, socket}
+            {:error, changeset} ->
+              {:reply, {:error, changeset}, socket}
+          end
+        rescue
+          Ecto.NoResultsError ->
+            {:reply, {:error, ErrorView.render("404.json")}, socket}
+        end
+    end
   end
 
-  def handle_in("delete", _payload, _socket) do
+  def handle_in("delete", payload, socket) do
+    cond do
+      is_nil(payload["id"]) ->
+        {:reply, {:error, ErrorView.render("404.json")}, socket}
+      true ->
+        case Repo.get(Room, payload["id"]) do
+          nil ->
+            {:reply, {:error, ErrorView.render("404.json")}, socket}
+          room ->
+            case Repo.delete(room) do
+              {:ok, struct} ->
+                response = RoomView.render("show.json", %{rooms: struct})
+                {:reply, {:ok, %{deleted: response}}, socket}
+              {:error, changeset} ->
+                {:reply, {:error, changeset}, socket}
+            end
+        end
+    end
   end
 
-  def handle_info({:after_join, "rooms:lobby"}, socket) do
-    response =
-      Talk.list_room
-      |> Enum.map(&%{
-        description: &1.description,
-        name: &1.name,
-        topic: &1.topic,
-        user_id: &1.user_id
-      })
+#  def handle_info({:after_join, "rooms:lobby"}, socket) do
+#    response =
+#      Talk.list_room
+#      |> Enum.map(&%{
+#        description: &1.description,
+#        name: &1.name,
+#        topic: &1.topic,
+#        user_id: &1.user_id
+#      })
+#
+#    broadcast!(socket, "rooms:lobby", %{data: response})
+#    Endpoint.broadcast("rooms:lobby", "index", %{data: response})
+#    {:reply, {:ok, response}, socket}
+#  end
 
-    broadcast!(socket, "rooms:lobby", %{data: response})
-    Endpoint.broadcast("rooms:lobby", "index", %{data: response})
-    {:reply, {:ok, response}, socket}
-  end
-
-  def handle_out(event, payload, socket) do
-    push(socket, event, payload)
-    {:noreply, socket}
-  end
+#  def handle_out(event, payload, socket) do
+#    push(socket, event, payload)
+#    {:noreply, socket}
+#  end
 
 #  def join("room:lobby", _message, socket) do
 #    {:ok, socket}
@@ -111,11 +152,12 @@ defmodule ServerWeb.RoomChannel do
 # socket_opts = [url: "ws://localhost:4000/socket/websocket"]
 # {:ok, socket} = PhoenixClient.Socket.start_link(socket_opts)
 # {:ok, _response, channel} = PhoenixClient.Channel.join(socket, "rooms:lobby")
-# {:ok, data} = PhoenixClient.Channel.push(channel, "index", nil)
+# {:ok, data} = PhoenixClient.Channel.push(channel, "index", %{})
 # {:ok, data} = PhoenixClient.Channel.push(channel, "show", %{"id" => "9vsgUj0rX2aH8Iosgi"})
 # payload = %{"name" => "proba", "description" => "proba", "topic" => "proba", "user_id" => "9vwwJYHquq8GG7wkyW"}
 # {:ok, data} = PhoenixClient.Channel.push(channel, "create", payload)
-# {:ok, data} = PhoenixClient.Channel.push(channel, "update", %{"id" => "9vsgUj0rX2aH8Iosgi"})
+# payload = %{"id" => "9vwwcPdhuzhHAaqr4K", "name" => "updated", "description" => "updated", "topic" => "updated", "user_id" => "9vwwJYHquq8GG7wkyW"}
+# {:ok, data} = PhoenixClient.Channel.push(channel, "update", payload)
 # {:ok, data} = PhoenixClient.Channel.push(channel, "delete", %{"id" => "9vsgUj0rX2aH8Iosgi"})
 # ServerWeb.Endpoint.broadcast("room:lobby", "index", data)
 end
