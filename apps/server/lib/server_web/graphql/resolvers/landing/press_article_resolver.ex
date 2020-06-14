1f40ff95e716502c754af057c68bfb680f148390
Oleg G.Kapranov <ok@pure-agency.co>
2020-06-14 09:01:49 -1000

    fixed returns error PressArticle

--- apps/server/lib/server_web/graphql/resolvers/landing/press_article_resolver.ex
+++ apps/server/lib/server_web/graphql/resolvers/landing/press_article_resolver.ex
@@ -38,15 +38,24 @@ defmodule ServerWeb.GraphQL.Resolvers.Landing.PressArticleResolver do
     end
   end
 
+  @spec show(any, %{atom => any}, Absinthe.Resolution.t()) :: error_tuple()
+  def show(_parent, _args, _info) do
+    {:error, [[field: :id, message: "Can't be blank"]]}
+  end
+
   @spec create(any, %{atom => any}, Absinthe.Resolution.t()) :: result()
   def create(_parent, args, _info) do
-    args
-    |> Landing.create_press_article()
-    |> case do
-      {:ok, struct} ->
-        {:ok, struct}
-      {:error, changeset} ->
-        {:error, extract_error_msg(changeset)}
+    if is_nil(args[:author]) || is_nil(args[:img_url]) || is_nil(args[:preview_text]) || is_nil(args[:title]) || is_nil(args[:url]) do
+      {:error, [[field: :author, message: "author, img_url, preview_text, title, url can't be blank"]]}
+    else
+      args
+      |> Landing.create_press_article()
+      |> case do
+        {:ok, struct} ->
+          {:ok, struct}
+        {:error, changeset} ->
+          {:error, extract_error_msg(changeset)}
+      end
     end
   end
 
@@ -66,6 +75,11 @@ defmodule ServerWeb.GraphQL.Resolvers.Landing.PressArticleResolver do
     end
   end
 
+  @spec update(any, %{atom => any}, Absinthe.Resolution.t()) :: error_tuple()
+  def update(_parent, _args, _info) do
+    {:error, [[field: :id, message: "id and press_article params can't be blank"]]}
+  end
+
   @spec delete(any, %{id: bitstring}, Absinthe.Resolution.t()) :: result()
   def delete(_parent, %{id: id}, _info) do
     if is_nil(id) do
@@ -79,6 +93,11 @@ defmodule ServerWeb.GraphQL.Resolvers.Landing.PressArticleResolver do
           {:error, "The Press Article #{id} not found!"}
       end
     end
+  end
+
+  @spec delete(any, %{atom => any}, Absinthe.Resolution.t()) :: error_tuple()
+  def delete(_parent, _args, _info) do
+    {:error, [[field: :id, message: "Can't be blank"]]}
   end
 
   @spec extract_error_msg(Ecto.Changeset.t()) :: Ecto.Changeset.t()
