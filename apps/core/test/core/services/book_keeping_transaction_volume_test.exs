988e9cd84fe1d2d0ddcb6712981b76dc00de0066
Oleg G.Kapranov <ok@pure-agency.co>
2020-07-13 07:39:04 -1000

    fixed BookKeepingTransactionVolumeTest

--- apps/core/test/core/services/book_keeping_transaction_volume_test.exs
+++ apps/core/test/core/services/book_keeping_transaction_volume_test.exs
@@ -53,12 +53,12 @@ defmodule Core.Services.BookKeepingTransactionVolumeTest do
       book_keeping = insert(:tp_book_keeping, user: user)
 
       params = %{
-        name: "some name",
+        name: "1-25",
         book_keeping_id: book_keeping.id
       }
 
       assert {:ok, %{} = book_keeping_transaction_volume} = Services.create_book_keeping_transaction_volume(params)
-      assert book_keeping_transaction_volume.name            == "some name"
+      assert book_keeping_transaction_volume.name            == :"1-25"
       assert book_keeping_transaction_volume.price           == nil
       assert book_keeping_transaction_volume.book_keeping_id == book_keeping.id
     end
@@ -89,33 +89,14 @@ defmodule Core.Services.BookKeepingTransactionVolumeTest do
       struct = insert(:tp_book_keeping_transaction_volume, book_keepings: book_keeping)
 
       params = %{
-        name: "updated name",
+        name: "76-199",
         book_keeping_id: book_keeping.id
       }
 
       assert {:ok, %BookKeepingTransactionVolume{} = updated} =
         Services.update_book_keeping_transaction_volume(struct, params)
 
-      assert updated.name            == "updated name"
-      assert updated.price           == nil
-      assert updated.book_keeping_id == book_keeping.id
-    end
-
-    test "update_book_keeping_transaction_volume/2 with valid data updates and ignore price, book_keeping_id by role's Tp" do
-      user = insert(:tp_user)
-      book_keeping = insert(:tp_book_keeping, user: user)
-      struct = insert(:tp_book_keeping_transaction_volume, book_keepings: book_keeping)
-
-      params = %{
-        name: "updated name",
-        price: 22,
-        book_keeping_id: book_keeping.id
-      }
-
-      assert {:ok, %BookKeepingTransactionVolume{} = updated} =
-        Services.update_book_keeping_transaction_volume(struct, params)
-
-      assert updated.name            == "updated name"
+      assert updated.name            == :"76-199"
       assert updated.price           == nil
       assert updated.book_keeping_id == book_keeping.id
     end
@@ -194,13 +175,13 @@ defmodule Core.Services.BookKeepingTransactionVolumeTest do
       book_keeping = insert(:pro_book_keeping, user: user)
 
       params = %{
-        name: "some name",
+        name: "1-25",
         price: 22,
         book_keeping_id: book_keeping.id
       }
 
       assert {:ok, %{} = book_keeping_transaction_volume} = Services.create_book_keeping_transaction_volume(params)
-      assert book_keeping_transaction_volume.name            == "some name"
+      assert book_keeping_transaction_volume.name            == :"1-25"
       assert book_keeping_transaction_volume.price           == 22
       assert book_keeping_transaction_volume.book_keeping_id == book_keeping.id
     end
@@ -217,7 +198,7 @@ defmodule Core.Services.BookKeepingTransactionVolumeTest do
       struct = insert(:pro_book_keeping_transaction_volume, book_keepings: book_keeping)
 
       params = %{
-        name: "updated name",
+        name: "76-199",
         price: 33,
         book_keeping_id: book_keeping.id
       }
@@ -225,7 +206,7 @@ defmodule Core.Services.BookKeepingTransactionVolumeTest do
       assert {:ok, %BookKeepingTransactionVolume{} = updated} =
         Services.update_book_keeping_transaction_volume(struct, params)
 
-      assert updated.name            == "updated name"
+      assert updated.name            == :"76-199"
       assert updated.price           == 33
       assert updated.book_keeping_id == book_keeping.id
     end
