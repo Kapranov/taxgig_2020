2aed33fe05255474722d0640519bee8129a9f43e
Oleg G.Kapranov <ok@pure-agency.co>
2020-07-13 23:23:27 -1000

    fixed SaleTaxFrequencyTest

--- apps/core/test/core/services/sale_tax_frequency_test.exs
+++ apps/core/test/core/services/sale_tax_frequency_test.exs
@@ -52,27 +52,15 @@ defmodule Core.Services.SaleTaxFrequencyTest do
       sale_tax = insert(:tp_sale_tax, user: user)
 
       params = %{
-        name: "some name",
+        name: "Annually",
         sale_tax_id: sale_tax.id
       }
 
       assert {:ok, %SaleTaxFrequency{} = sale_tax_frequency} = Services.create_sale_tax_frequency(params)
-      assert sale_tax_frequency.name                         == "some name"
+      assert sale_tax_frequency.name                         == :Annually
       assert sale_tax_frequency.price                        == nil
       assert sale_tax_frequency.sale_tax_id                  == sale_tax.id
       assert match_value_relate.match_for_sale_tax_frequency == 10
-    end
-
-    test "create_sale_tax_frequency/1 with invalid data with price by role's Tp returns error changeset" do
-      user = insert(:tp_user)
-      sale_tax = insert(:tp_sale_tax, user: user)
-
-      params = %{
-        name: "some name",
-        price: 22,
-        sale_tax_id: sale_tax.id
-      }
-      assert {:error, %Ecto.Changeset{}} = Services.create_sale_tax_frequency(params)
     end
 
     test "create_sale_tax_frequency/1 with invalid data returns error changeset" do
@@ -86,30 +74,21 @@ defmodule Core.Services.SaleTaxFrequencyTest do
       struct = insert(:tp_sale_tax_frequency, sale_taxes: sale_tax)
 
       params = %{
-        name: "updated name",
+        name: "Quarterly",
         sale_tax_id: sale_tax.id
       }
 
-      assert {
-        :error,
-        [
-          field: :name,
-          message: "name or sale_tax_id already is exist, not permission for update record"
-        ]
-      } == Services.update_sale_tax_frequency(struct, params)
+      assert {:ok, %SaleTaxFrequency{} = updated} =
+        Services.update_sale_tax_frequency(struct, params)
+      assert updated.name                         == :Quarterly
+      assert updated.price                        == nil
+      assert updated.sale_tax_id                  == sale_tax.id
     end
 
     test "update_sale_tax_frequency/2 with invalid data returns error changeset" do
-      user = insert(:tp_user)
-      sale_tax = insert(:tp_sale_tax, user: user)
-      struct = insert(:tp_sale_tax_frequency, sale_taxes: sale_tax)
+      struct = insert(:tp_sale_tax_frequency)
       params = %{name: nil, sale_tax_id: nil}
-      attrs = [:password, :password_cofirmation]
-      data = Services.get_sale_tax_frequency!(struct.id)
-      assert {:error, [field: :id, message: "record already is exist, not permission for new record"]} =
-        Services.update_sale_tax_frequency(struct, params)
-      assert {:error, %Ecto.Changeset{}} != Services.update_sale_tax_frequency(struct, params)
-      assert Map.take(struct, attrs) == assert Map.take(data, attrs)
+      assert {:error, %Ecto.Changeset{}} == Services.update_sale_tax_frequency(struct, params)
     end
 
     test "delete_sale_tax_frequency/1 deletes the sale_tax_frequency" do
@@ -174,13 +153,13 @@ defmodule Core.Services.SaleTaxFrequencyTest do
       sale_tax = insert(:pro_sale_tax, user: user)
 
       params = %{
-        name: "some name",
+        name: "Annually",
         price: 22,
         sale_tax_id: sale_tax.id
       }
 
       assert {:ok, %SaleTaxFrequency{} = sale_tax_frequency} = Services.create_sale_tax_frequency(params)
-      assert sale_tax_frequency.name                         == "some name"
+      assert sale_tax_frequency.name                         == :Annually
       assert sale_tax_frequency.price                        == 22
       assert sale_tax_frequency.sale_tax_id                  == sale_tax.id
       assert match_value_relate.match_for_sale_tax_frequency == 10
@@ -198,28 +177,23 @@ defmodule Core.Services.SaleTaxFrequencyTest do
       struct = insert(:pro_sale_tax_frequency, sale_taxes: sale_tax)
 
       params = %{
-        name: "updated name",
+        name: "Quarterly",
         price: 33,
         sale_tax_id: sale_tax.id
       }
 
       assert {:ok, %SaleTaxFrequency{} = updated} =
         Services.update_sale_tax_frequency(struct, params)
-      assert updated.name                         == "updated name"
+      assert updated.name                         == :Quarterly
       assert updated.price                        == 33
       assert updated.sale_tax_id                  == sale_tax.id
       assert match_value_relate.match_for_sale_tax_frequency == 10
     end
 
     test "update_sale_tax_frequency/2 with invalid data returns error changeset" do
-      user = insert(:pro_user)
-      sale_tax = insert(:pro_sale_tax, user: user)
-      struct = insert(:pro_sale_tax_frequency, sale_taxes: sale_tax)
+      struct = insert(:pro_sale_tax_frequency)
       params = %{name: nil, sale_tax_id: nil}
-      attrs = [:password, :password_cofirmation]
-      data = Services.get_sale_tax_frequency!(struct.id)
-      assert {:error, %Ecto.Changeset{}} != Services.update_sale_tax_frequency(struct, params)
-      assert Map.take(struct, attrs) == assert Map.take(data, attrs)
+      assert {:error, %Ecto.Changeset{}} == Services.update_sale_tax_frequency(struct, params)
     end
 
     test "delete_sale_tax_frequency/1 deletes the sale_tax_frequency" do
