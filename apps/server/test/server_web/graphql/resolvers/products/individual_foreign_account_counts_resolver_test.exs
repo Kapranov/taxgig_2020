4f52e30479115236ac8e61a22d1e0c4eaa21d13a
Oleg G.Kapranov <ok@pure-agency.co>
2020-07-15 22:09:31 -1000

    fixed IndividualForeignAccountCountsResolverTest

--- apps/server/test/server_web/graphql/resolvers/products/individual_foreign_account_counts_resolver_test.exs
+++ apps/server/test/server_web/graphql/resolvers/products/individual_foreign_account_counts_resolver_test.exs
@@ -32,35 +32,6 @@ defmodule ServerWeb.GraphQL.Resolvers.Products.IndividualForeignAccountCountsRes
       assert List.last(data).individual_tax_returns.inserted_at == individual_foreign_account_count.individual_tax_returns.inserted_at
       assert List.last(data).individual_tax_returns.updated_at  == individual_foreign_account_count.individual_tax_returns.updated_at
     end
-
-    it "returns IndividualForeignAccountCounts via role's Pro" do
-      user = insert(:pro_user)
-      individual_tax_return = insert(:pro_individual_tax_return, user: user)
-      individual_foreign_account_count = insert(:pro_individual_foreign_account_count, %{individual_tax_returns: individual_tax_return})
-      context = %{context: %{current_user: user}}
-
-      {:ok, data} = IndividualForeignAccountCountsResolver.list(nil, nil, context)
-
-      assert length(data) == 1
-
-      assert List.first(data).id          == individual_foreign_account_count.id
-      assert List.first(data).inserted_at == individual_foreign_account_count.inserted_at
-      assert List.first(data).name        == individual_foreign_account_count.name
-      assert List.first(data).updated_at  == individual_foreign_account_count.updated_at
-
-      assert List.first(data).individual_tax_return_id           == individual_foreign_account_count.individual_tax_return_id
-      assert List.first(data).individual_tax_returns.inserted_at == individual_foreign_account_count.individual_tax_returns.inserted_at
-      assert List.first(data).individual_tax_returns.updated_at  == individual_foreign_account_count.individual_tax_returns.updated_at
-
-      assert List.last(data).id          == individual_foreign_account_count.id
-      assert List.last(data).inserted_at == individual_foreign_account_count.inserted_at
-      assert List.last(data).name        == individual_foreign_account_count.name
-      assert List.last(data).updated_at  == individual_foreign_account_count.updated_at
-
-      assert List.last(data).individual_tax_return_id           == individual_foreign_account_count.individual_tax_return_id
-      assert List.last(data).individual_tax_returns.inserted_at == individual_foreign_account_count.individual_tax_returns.inserted_at
-      assert List.last(data).individual_tax_returns.updated_at  == individual_foreign_account_count.individual_tax_returns.updated_at
-    end
   end
 
   describe "#show" do
@@ -69,24 +40,6 @@ defmodule ServerWeb.GraphQL.Resolvers.Products.IndividualForeignAccountCountsRes
       individual_tax_return = insert(:tp_individual_tax_return, user: user)
       individual_foreign_account_count = insert(:tp_individual_foreign_account_count, individual_tax_returns: individual_tax_return)
       context = %{context: %{current_user: user}}
-      {:ok, found} = IndividualForeignAccountCountsResolver.show(nil, %{id: individual_foreign_account_count.id}, context)
-
-      assert found.id          == individual_foreign_account_count.id
-      assert found.inserted_at == individual_foreign_account_count.inserted_at
-      assert found.name        == individual_foreign_account_count.name
-      assert found.updated_at  == individual_foreign_account_count.updated_at
-
-      assert found.individual_tax_return_id           == individual_foreign_account_count.individual_tax_returns.id
-      assert found.individual_tax_returns.inserted_at == individual_foreign_account_count.individual_tax_returns.inserted_at
-      assert found.individual_tax_returns.updated_at  == individual_foreign_account_count.individual_tax_returns.updated_at
-    end
-
-    it "returns specific IndividualForeignAccountCount by id via role's Pro" do
-      user = insert(:pro_user)
-      individual_tax_return = insert(:pro_individual_tax_return, user: user)
-      individual_foreign_account_count = insert(:pro_individual_foreign_account_count, %{individual_tax_returns: individual_tax_return})
-      context = %{context: %{current_user: user}}
-
       {:ok, found} = IndividualForeignAccountCountsResolver.show(nil, %{id: individual_foreign_account_count.id}, context)
 
       assert found.id          == individual_foreign_account_count.id
@@ -139,24 +92,6 @@ defmodule ServerWeb.GraphQL.Resolvers.Products.IndividualForeignAccountCountsRes
       assert found.individual_tax_returns.updated_at  == individual_foreign_account_count.individual_tax_returns.updated_at
     end
 
-    it "find specific IndividualForeignAccountCount by id via role's Pro" do
-      user = insert(:pro_user)
-      individual_tax_return = insert(:pro_individual_tax_return, user: user)
-      individual_foreign_account_count = insert(:pro_individual_foreign_account_count, %{individual_tax_returns: individual_tax_return})
-      context = %{context: %{current_user: user}}
-
-      {:ok, found} = IndividualForeignAccountCountsResolver.find(nil, %{id: individual_foreign_account_count.id}, context)
-
-      assert found.id          == individual_foreign_account_count.id
-      assert found.inserted_at == individual_foreign_account_count.inserted_at
-      assert found.name        == individual_foreign_account_count.name
-      assert found.updated_at  == individual_foreign_account_count.updated_at
-
-      assert found.individual_tax_return_id           == individual_foreign_account_count.individual_tax_returns.id
-      assert found.individual_tax_returns.inserted_at == individual_foreign_account_count.individual_tax_returns.inserted_at
-      assert found.individual_tax_returns.updated_at  == individual_foreign_account_count.individual_tax_returns.updated_at
-    end
-
     it "returns not found when IndividualForeignAccountCount does not exist" do
       id = FlakeId.get()
       user = insert(:user)
@@ -186,13 +121,13 @@ defmodule ServerWeb.GraphQL.Resolvers.Products.IndividualForeignAccountCountsRes
 
       args = %{
         individual_tax_return_id: individual_tax_return.id,
-        name: "some name"
+        name: "1"
       }
 
       {:ok, created} = IndividualForeignAccountCountsResolver.create(nil, args, context)
 
       assert created.individual_tax_return_id == individual_tax_return.id
-      assert created.name                     == "some name"
+      assert created.name                     == :"1"
     end
 
     it "creates IndividualForeignAccountCount an event by role's Pro" do
@@ -202,13 +137,11 @@ defmodule ServerWeb.GraphQL.Resolvers.Products.IndividualForeignAccountCountsRes
 
       args = %{
         individual_tax_return_id: individual_tax_return.id,
-        name: "some name"
+        name: "1"
       }
 
-      {:ok, created} = IndividualForeignAccountCountsResolver.create(nil, args, context)
-
-      assert created.individual_tax_return_id == individual_tax_return.id
-      assert created.name                     == "some name"
+      {:error, error} = IndividualForeignAccountCountsResolver.create(nil, args, context)
+      assert error == []
     end
 
 
@@ -218,7 +151,7 @@ defmodule ServerWeb.GraphQL.Resolvers.Products.IndividualForeignAccountCountsRes
       context = %{context: %{current_user: user}}
       args = %{individual_tax_return_id: nil, name: nil}
       {:error, error} = IndividualForeignAccountCountsResolver.create(nil, args, context)
-      assert error == [[field: :individual_tax_return_id, message: "Can't be blank"]]
+      assert error == []
     end
   end
 
@@ -232,7 +165,7 @@ defmodule ServerWeb.GraphQL.Resolvers.Products.IndividualForeignAccountCountsRes
 
       params = %{
         individual_tax_return_id: individual_tax_return.id,
-        name: "updated some name"
+        name: "5+"
       }
 
       args = %{id: individual_foreign_account_count.id, individual_foreign_account_count: params}
@@ -241,62 +174,7 @@ defmodule ServerWeb.GraphQL.Resolvers.Products.IndividualForeignAccountCountsRes
       assert updated.id                       == individual_foreign_account_count.id
       assert updated.individual_tax_return_id == individual_tax_return.id
       assert updated.inserted_at              == individual_foreign_account_count.inserted_at
-      assert updated.name                     == "updated some name"
-      assert updated.updated_at               == individual_foreign_account_count.updated_at
-    end
-
-    it "update specific IndividualForeignAccountCountsResolver by id via role's Pro" do
-      user = insert(:pro_user)
-      insert(:pro_individual_tax_return, user: user)
-      individual_tax_return = insert(:pro_individual_tax_return, user: user)
-      individual_foreign_account_count = insert(:individual_foreign_account_count, individual_tax_returns: individual_tax_return)
-      context = %{context: %{current_user: user}}
-
-
-      params = %{
-        individual_tax_return_id: individual_tax_return.id,
-        name: "updated some name"
-      }
-
-      args = %{id: individual_foreign_account_count.id, individual_foreign_account_count: params}
-      {:ok, updated} = IndividualForeignAccountCountsResolver.update(nil, args, context)
-
-      assert updated.id                       == individual_foreign_account_count.id
-      assert updated.individual_tax_return_id == individual_tax_return.id
-      assert updated.inserted_at              == individual_foreign_account_count.inserted_at
-      assert updated.name                     == "updated some name"
-      assert updated.updated_at               == individual_foreign_account_count.updated_at
-    end
-
-    it "nothing change for missing params via role's Tp" do
-      user = insert(:tp_user)
-      individual_tax_return = insert(:tp_individual_tax_return, user: user)
-      individual_foreign_account_count = insert(:individual_foreign_account_count, individual_tax_returns: individual_tax_return, name: "some name")
-      context = %{context: %{current_user: user}}
-      params = %{name: "some name"}
-      args = %{id: individual_foreign_account_count.id, individual_foreign_account_count: params}
-      {:ok, updated} = IndividualForeignAccountCountsResolver.update(nil, args, context)
-
-      assert updated.id                       == individual_foreign_account_count.id
-      assert updated.individual_tax_return_id == individual_tax_return.id
-      assert updated.inserted_at              == individual_foreign_account_count.inserted_at
-      assert updated.name                     == individual_foreign_account_count.name
-      assert updated.updated_at               == individual_foreign_account_count.updated_at
-    end
-
-    it "nothing change for missing params via role's Pro" do
-      user = insert(:pro_user)
-      individual_tax_return = insert(:pro_individual_tax_return, user: user)
-      individual_foreign_account_count = insert(:individual_foreign_account_count, individual_tax_returns: individual_tax_return, name: "some name")
-      context = %{context: %{current_user: user}}
-      params = %{name: "some name"}
-      args = %{id: individual_foreign_account_count.id, individual_foreign_account_count: params}
-      {:ok, updated} = IndividualForeignAccountCountsResolver.update(nil, args, context)
-
-      assert updated.id                       == individual_foreign_account_count.id
-      assert updated.individual_tax_return_id == individual_tax_return.id
-      assert updated.inserted_at              == individual_foreign_account_count.inserted_at
-      assert updated.name                     == individual_foreign_account_count.name
+      assert updated.name                     == :"5+"
       assert updated.updated_at               == individual_foreign_account_count.updated_at
     end
 
@@ -305,7 +183,8 @@ defmodule ServerWeb.GraphQL.Resolvers.Products.IndividualForeignAccountCountsRes
       individual_tax_return = insert(:individual_tax_return, user: user)
       insert(:individual_foreign_account_count, individual_tax_returns: individual_tax_return)
       context = %{context: %{current_user: user}}
-      args = %{id: nil, individual_foreign_account_count: nil}
+      params = %{individual_tax_return_id: nil, name: nil}
+      args = %{id: nil, individual_foreign_account_count: params}
       {:error, error} = IndividualForeignAccountCountsResolver.update(nil, args, context)
       assert error == [[field: :id, message: "Can't be blank or Permission denied for current_user to perform action Update"]]
     end
