d59a3c3f2a64150ae24d1377f3536c1854414d1d
Oleg G.Kapranov <ok@pure-agency.co>
2020-07-15 21:58:58 -1000

    fixed IndividualEmploymentStatusesResolverTest

--- apps/server/test/server_web/graphql/resolvers/products/individual_employment_statuses_resolver_test.exs
+++ apps/server/test/server_web/graphql/resolvers/products/individual_employment_statuses_resolver_test.exs
@@ -17,6 +17,7 @@ defmodule ServerWeb.GraphQL.Resolvers.Products.IndividualEmploymentStatusesResol
       assert List.first(data).id          == individual_employment_status.id
       assert List.first(data).inserted_at == individual_employment_status.inserted_at
       assert List.first(data).name        == individual_employment_status.name
+      assert List.first(data).price       == nil
       assert List.first(data).updated_at  == individual_employment_status.updated_at
 
       assert List.first(data).individual_tax_return_id           == individual_employment_status.individual_tax_return_id
@@ -26,7 +27,7 @@ defmodule ServerWeb.GraphQL.Resolvers.Products.IndividualEmploymentStatusesResol
       assert List.last(data).id          == individual_employment_status.id
       assert List.last(data).inserted_at == individual_employment_status.inserted_at
       assert List.last(data).name        == individual_employment_status.name
-      assert List.last(data).price       == individual_employment_status.price
+      assert List.last(data).price       == nil
       assert List.last(data).updated_at  == individual_employment_status.updated_at
 
       assert List.last(data).individual_tax_return_id           == individual_employment_status.individual_tax_return_id
@@ -76,6 +77,7 @@ defmodule ServerWeb.GraphQL.Resolvers.Products.IndividualEmploymentStatusesResol
       assert found.id          == individual_employment_status.id
       assert found.inserted_at == individual_employment_status.inserted_at
       assert found.name        == individual_employment_status.name
+      assert found.price       == nil
       assert found.updated_at  == individual_employment_status.updated_at
 
       assert found.individual_tax_return_id           == individual_employment_status.individual_tax_returns.id
@@ -135,6 +137,7 @@ defmodule ServerWeb.GraphQL.Resolvers.Products.IndividualEmploymentStatusesResol
       assert found.id          == individual_employment_status.id
       assert found.inserted_at == individual_employment_status.inserted_at
       assert found.name        == individual_employment_status.name
+      assert found.price       == nil
       assert found.updated_at  == individual_employment_status.updated_at
 
       assert found.individual_tax_return_id           == individual_employment_status.individual_tax_returns.id
@@ -190,13 +193,14 @@ defmodule ServerWeb.GraphQL.Resolvers.Products.IndividualEmploymentStatusesResol
 
       args = %{
         individual_tax_return_id: individual_tax_return.id,
-        name: "some name"
+        name: "employed"
       }
 
       {:ok, created} = IndividualEmploymentStatusesResolver.create(nil, args, context)
 
       assert created.individual_tax_return_id == individual_tax_return.id
-      assert created.name                     == "some name"
+      assert created.name                     == :employed
+      assert created.price                    == nil
     end
 
     it "creates IndividualEmploymentStatus an event by role's Pro" do
@@ -206,14 +210,14 @@ defmodule ServerWeb.GraphQL.Resolvers.Products.IndividualEmploymentStatusesResol
 
       args = %{
         individual_tax_return_id: individual_tax_return.id,
-        name: "some name",
+        name: "employed",
         price: 12
       }
 
       {:ok, created} = IndividualEmploymentStatusesResolver.create(nil, args, context)
 
       assert created.individual_tax_return_id == individual_tax_return.id
-      assert created.name                     == "some name"
+      assert created.name                     == :employed
       assert created.price                    == 12
     end
 
@@ -232,12 +236,12 @@ defmodule ServerWeb.GraphQL.Resolvers.Products.IndividualEmploymentStatusesResol
       user = insert(:tp_user)
       insert(:tp_individual_tax_return, user: user)
       new_individual_tax_return = insert(:tp_individual_tax_return, user: user)
-      individual_employment_status = insert(:individual_employment_status, individual_tax_returns: new_individual_tax_return)
+      individual_employment_status = insert(:tp_individual_employment_status, individual_tax_returns: new_individual_tax_return)
       context = %{context: %{current_user: user}}
 
       params = %{
         individual_tax_return_id: new_individual_tax_return.id,
-        name: "updated some name"
+        name: "unemployed"
       }
 
       args = %{id: individual_employment_status.id, individual_employment_status: params}
@@ -246,7 +250,8 @@ defmodule ServerWeb.GraphQL.Resolvers.Products.IndividualEmploymentStatusesResol
       assert updated.id                       == individual_employment_status.id
       assert updated.individual_tax_return_id == new_individual_tax_return.id
       assert updated.inserted_at              == individual_employment_status.inserted_at
-      assert updated.name                     == "updated some name"
+      assert updated.name                     == :unemployed
+      assert updated.price                    == nil
       assert updated.updated_at               == individual_employment_status.updated_at
     end
 
@@ -254,12 +259,12 @@ defmodule ServerWeb.GraphQL.Resolvers.Products.IndividualEmploymentStatusesResol
       user = insert(:pro_user)
       insert(:pro_individual_tax_return, user: user)
       new_individual_tax_return = insert(:pro_individual_tax_return, user: user)
-      individual_employment_status = insert(:individual_employment_status, individual_tax_returns: new_individual_tax_return)
+      individual_employment_status = insert(:pro_individual_employment_status, individual_tax_returns: new_individual_tax_return)
       context = %{context: %{current_user: user}}
 
       params = %{
         individual_tax_return_id: new_individual_tax_return.id,
-        name: "updated some name",
+        name: "unemployed",
         price: 13
       }
 
@@ -269,39 +274,8 @@ defmodule ServerWeb.GraphQL.Resolvers.Products.IndividualEmploymentStatusesResol
       assert updated.id                       == individual_employment_status.id
       assert updated.individual_tax_return_id == new_individual_tax_return.id
       assert updated.inserted_at              == individual_employment_status.inserted_at
-      assert updated.name                     == "updated some name"
+      assert updated.name                     == :unemployed
       assert updated.price                    == 13
-      assert updated.updated_at               == individual_employment_status.updated_at
-    end
-
-    it "nothing change for missing params via role's Tp" do
-      user = insert(:tp_user)
-      individual_tax_return = insert(:tp_individual_tax_return, user: user)
-      individual_employment_status = insert(:individual_employment_status, individual_tax_returns: individual_tax_return, name: "some name")
-      context = %{context: %{current_user: user}}
-      params = %{name: "some name"}
-      args = %{id: individual_employment_status.id, individual_employment_status: params}
-      {:ok, updated} = IndividualEmploymentStatusesResolver.update(nil, args, context)
-
-      assert updated.id                       == individual_employment_status.id
-      assert updated.individual_tax_return_id == individual_tax_return.id
-      assert updated.inserted_at              == individual_employment_status.inserted_at
-      assert updated.name                     == individual_employment_status.name
-      assert updated.updated_at               == individual_employment_status.updated_at
-    end
-
-    it "nothing change for missing params via role's Pro" do
-      user = insert(:pro_user)
-      individual_tax_return = insert(:pro_individual_tax_return, user: user)
-      individual_employment_status = insert(:individual_employment_status, individual_tax_returns: individual_tax_return, price: 12)
-      context = %{context: %{current_user: user}}
-      params = %{price: 12}
-      args = %{id: individual_employment_status.id, individual_employment_status: params}
-      {:ok, updated} = IndividualEmploymentStatusesResolver.update(nil, args, context)
-
-      assert updated.id                       == individual_employment_status.id
-      assert updated.individual_tax_return_id == individual_tax_return.id
-      assert updated.inserted_at              == individual_employment_status.inserted_at
       assert updated.updated_at               == individual_employment_status.updated_at
     end
 
@@ -310,7 +284,8 @@ defmodule ServerWeb.GraphQL.Resolvers.Products.IndividualEmploymentStatusesResol
       individual_tax_return = insert(:individual_tax_return, user: user)
       insert(:individual_employment_status, individual_tax_returns: individual_tax_return)
       context = %{context: %{current_user: user}}
-      args = %{id: nil, individual_employment_status: nil}
+      params = %{individual_tax_return_id: nil, name: nil}
+      args = %{id: nil, individual_employment_status: params}
       {:error, error} = IndividualEmploymentStatusesResolver.update(nil, args, context)
       assert error == [[field: :id, message: "Can't be blank or Permission denied for current_user to perform action Update"]]
     end
