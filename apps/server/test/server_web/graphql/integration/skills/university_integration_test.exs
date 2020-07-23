084df8692a422166714344ad8832d44c1b85450a
Oleg G.Kapranov <ok@pure-agency.co>
2020-07-23 08:49:01 -1000

    added EducationIntegrationTest

--- apps/server/test/server_web/graphql/integration/skills/university_integration_test.exs
+++ apps/server/test/server_web/graphql/integration/skills/university_integration_test.exs
@@ -6,48 +6,251 @@ defmodule ServerWeb.GraphQL.Integration.Skills.UniversityIntegrationTest do
 
   describe "#list" do
     it "returns universities - `AbsintheHelpers`" do
+      struct = insert(:university)
+
+      query = """
+      {
+        allUniversities {
+          id
+          name
+        }
+      }
+      """
+      res =
+        build_conn()
+        |> post("/graphiql", AbsintheHelpers.query_skeleton(query, "allUniversities"))
+
+      assert json_response(res, 200)["errors"] == nil
+
+      data = json_response(res, 200)["data"]["allUniversities"]
+
+      assert List.first(data)["id"]   == struct.id
+      assert List.first(data)["name"] == struct.name
+
+      assert List.last(data)["id"]   == struct.id
+      assert List.last(data)["name"] == struct.name
     end
 
     it "returns universities - `Absinthe.run`" do
+      struct = insert(:university)
+      context = %{}
+
+      query = """
+      {
+        allUniversities {
+          id
+          name
+        }
+      }
+      """
+
+      {:ok, %{data: %{"allUniversities" => data}}} =
+        Absinthe.run(query, Schema, context: context)
+
+      first = hd(data)
+
+      assert first["id"]   == struct.id
+      assert first["name"] == struct.name
     end
   end
 
   describe "#show" do
     it "returns specific university by id - `AbsintheHelpers`" do
+      struct = insert(:university)
+
+      query = """
+      {
+        showUniversity(id: \"#{struct.id}\") {
+          id
+          name
+        }
+      }
+      """
+
+      res =
+        build_conn()
+        |> post("/graphiql", AbsintheHelpers.query_skeleton(query, "showUniversity"))
+
+      assert json_response(res, 200)["errors"] == nil
+
+      found = json_response(res, 200)["data"]["showUniversity"]
+
+      assert found["id"]   == struct.id
+      assert found["name"] == struct.name
     end
 
     it "returns specific university by id - `Absinthe.run`" do
+      struct = insert(:university)
+      context = %{}
+
+      query = """
+      {
+        showUniversity(id: \"#{struct.id}\") {
+          id
+          name
+        }
+      }
+      """
+
+      {:ok, %{data: %{"showUniversity" => found}}} =
+        Absinthe.run(query, Schema, context: context)
+
+      assert found["id"]   == struct.id
+      assert found["name"] == struct.name
     end
 
     it "returns not found when university does not exist - `AbsintheHelpers`" do
+      id = FlakeId.get()
+      insert(:university)
+
+      query = """
+      {
+        showUniversity(id: \"#{id}\") {
+          id
+          name
+        }
+      }
+      """
+
+      res =
+        build_conn()
+        |> post("/graphiql", AbsintheHelpers.query_skeleton(query, "showUniversity"))
+
+      assert hd(json_response(res, 200)["errors"])["message"] == "The University #{id} not found!"
     end
 
     it "returns not found when university does not exist - `Absinthe.run`" do
+      id = FlakeId.get()
+      insert(:university)
+      context = %{}
+
+      query = """
+      {
+        showUniversity(id: \"#{id}\") {
+          id
+          name
+        }
+      }
+      """
+
+      {:ok, %{data: %{"showUniversity" => found}}} =
+        Absinthe.run(query, Schema, context: context)
+
+      assert found == nil
     end
 
     it "returns error for missing params - `AbsintheHelpers`" do
+      insert(:university)
+
+      query = """
+      {
+        showUniversity(id: nil) {
+          id
+          name
+        }
+      }
+      """
+
+      res =
+        build_conn()
+        |> post("/graphiql", AbsintheHelpers.query_skeleton(query, "showUniversity"))
+
+      assert hd(json_response(res, 200)["errors"])["message"] == "Argument \"id\" has invalid value nil."
     end
 
     it "returns error for missing params - `Absinthe.run`" do
+      insert(:university)
+      context = %{}
+
+      query = """
+      {
+        showUniversity(id: nil) {
+          id
+          name
+        }
+      }
+      """
+
+      {:ok, %{errors: error}} =
+        Absinthe.run(query, Schema, context: context)
+
+      assert hd(error).message == "Argument \"id\" has invalid value nil."
     end
   end
 
   describe "#create" do
     it "creates university - `AbsintheHelpers`" do
+      query = """
+      mutation {createUniversity(name: "some text") { id name }}
+      """
+
+      res =
+        build_conn()
+        |> post("/graphiql", AbsintheHelpers.mutation_skeleton(query))
+
+      assert json_response(res, 200)["errors"] == nil
+
+      created = json_response(res, 200)["data"]["createUniversity"]
+
+      assert created["name"] == "some text"
     end
 
     it "creates university - `Absinthe.run`" do
+      context = %{}
+
+      query = """
+      mutation {createUniversity(name: "some text") { id name }}
+      """
+
+      {:ok, %{data: %{"createUniversity" => created}}} =
+        Absinthe.run(query, Schema, context: context)
+
+      assert created["name"] == "some text"
     end
 
     it "returns error for missing params - `AbsintheHelpers`" do
+      query = """
+      mutation {createUniversity(name: nil) { id name }}
+      """
+
+      res =
+        build_conn()
+        |> post("/graphiql", AbsintheHelpers.mutation_skeleton(query))
+
+      assert hd(json_response(res, 200)["errors"])["message"] == "Argument \"name\" has invalid value nil."
     end
 
     it "returns error for missing params - `Absinthe.run`" do
+      context = %{}
+
+      query = """
+      mutation {createUniversity(name: nil) { id name }}
+      """
+
+      {:ok, %{errors: error}} =
+        Absinthe.run(query, Schema, context: context)
+
+      assert hd(error).message == "Argument \"name\" has invalid value nil."
     end
   end
 
   describe "#delete" do
     it "delete specific university by id - `AbsintheHelpers`" do
+      struct = insert(:university)
+      user = Core.Accounts.User.find_by(id: struct.user_id)
+
+      query = """
+      mutation {deleteUniversity(id: \"#{struct.id}\") {id}}
+      """
+
+      res =
+        build_conn()
+        |> post("/graphiql", AbsintheHelpers.mutation_skeleton(query))
+
+      assert json_response(res, 200)["errors"] == nil
+
+      deleted = json_response(res, 200)["data"]["deleteUniversity"]
+      assert deleted["id"] == struct.id
     end
 
     it "delete specific university by id - `Absinthe.run`" do
