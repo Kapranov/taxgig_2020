4ddd7468b96b0a5e7578b5e52eca32eae871c9aa
Oleg G.Kapranov <ok@pure-agency.co>
2020-07-24 00:33:28 -1000

    updated update_sale_tax

--- apps/core/lib/core/services/services.ex
+++ apps/core/lib/core/services/services.ex
@@ -6302,49 +6302,21 @@ defmodule Core.Services do
   """
   @spec update_sale_tax(SaleTax.t(), %{atom => any}) :: result() | error_tuple()
   def update_sale_tax(%SaleTax{} = struct, attrs) do
-    get_role_by_user =
-      case struct.user_id do
-        nil -> nil
-        _ ->
-          Repo.one(
-            from c in User,
-            where: c.id == ^struct.user_id,
-            where: not is_nil(c.role),
-            select: c.role
-          )
+     tp_attrs = Map.drop(attrs, [:price_sale_tax_count])
+    pro_attrs = Map.drop(attrs, [:deadline, :financial_situation, :sale_tax_count, :state])
+    if attrs.user_id == struct.user_id do
+      case Accounts.get_user!(struct.user_id).role do
+        false ->
+          struct
+          |> SaleTax.changeset(tp_attrs)
+          |> Repo.update()
+        true ->
+          struct
+          |> SaleTax.changeset(pro_attrs)
+          |> Repo.update()
       end
-
-    query =
-      case struct.id do
-        nil -> nil
-        _ ->
-          from c in SaleTax,
-          where: c.id == ^struct.id,
-          select: c.id
-      end
-
-    tp_attrs =
-      attrs
-      |> Map.drop([:price_sale_tax_count])
-
-    pro_attrs =
-      attrs
-      |> Map.drop([:financial_situation, :deadline, :sale_tax_count, :state, :user_id])
-
-    case get_role_by_user do
-      nil -> {:error, %Changeset{}}
-      false ->
-        case Repo.aggregate(query, :count, :id) do
-          1 ->
-            struct
-            |> SaleTax.changeset(tp_attrs)
-            |> Repo.update()
-          _ -> {:error, [field: :id, message: "record already is exist, not permission for new record"]}
-        end
-      true ->
-        struct
-        |> SaleTax.changeset(pro_attrs)
-        |> Repo.update()
+    else
+      {:error, %Changeset{}}
     end
   end
 
