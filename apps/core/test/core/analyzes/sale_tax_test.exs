3406971039ceb3f9196ebab79884c71ad57457f5
Oleg G.Kapranov <ok@pure-agency.co>
2020-07-28 19:00:31 -1000

    updated Core.Analyzes.SaleTaxTest

--- apps/core/test/core/analyzes/sale_tax_test.exs
+++ apps/core/test/core/analyzes/sale_tax_test.exs
@@ -7,7 +7,103 @@ defmodule Core.Analyzes.SaleTaxTest do
   }
 
   describe "#check_match" do
-    test "check_match_sale_tax_count" do
+    test "check_match_sale_tax_count by role Tp" do
+      match = insert(:match_value_relat)
+      pro = insert(:pro_user)
+      tp = insert(:tp_user)
+      st_pro = insert(:pro_sale_tax, price_sale_tax_count: 33, user: pro)
+      st_tp = insert(:tp_sale_tax, sale_tax_count: 22, user: tp)
+      data = SaleTax.check_match_sale_tax_count(st_tp.id)
+      assert match.match_for_sale_tax_count  == 50
+      assert st_tp.sale_tax_count            == 22
+      assert st_pro.price_sale_tax_count     == 33
+      assert data                            == %{st_pro.id => match.match_for_sale_tax_count}
+    end
+
+    test "check_match_sale_tax_count if match_for_sale_tax_count is 0 by role Tp" do
+      match = insert(:match_value_relat, match_for_sale_tax_count: 0)
+      pro = insert(:pro_user)
+      tp = insert(:tp_user)
+      st_pro = insert(:pro_sale_tax, price_sale_tax_count: 33, user: pro)
+      st_tp = insert(:tp_sale_tax, sale_tax_count: 22, user: tp)
+      data = SaleTax.check_match_sale_tax_count(st_tp.id)
+      assert match.match_for_sale_tax_count  == 0
+      assert st_tp.sale_tax_count            == 22
+      assert st_pro.price_sale_tax_count     == 33
+      assert data                            == %{st_pro.id => match.match_for_sale_tax_count}
+    end
+
+    test "check_match_sale_tax_count if match_for_sale_tax_count is nil by role Tp" do
+      match = insert(:match_value_relat, match_for_sale_tax_count: nil)
+      pro = insert(:pro_user)
+      tp = insert(:tp_user)
+      st_pro = insert(:pro_sale_tax, price_sale_tax_count: 33, user: pro)
+      st_tp = insert(:tp_sale_tax, sale_tax_count: 22, user: tp)
+      data = SaleTax.check_match_sale_tax_count(st_tp.id)
+      assert match.match_for_sale_tax_count  == nil
+      assert st_tp.sale_tax_count            == 22
+      assert st_pro.price_sale_tax_count     == 33
+      assert data                            == %{st_pro.id => 0}
+    end
+
+    test "check_match_sale_tax_count if price_sale_tax_count is 0 by role Tp" do
+      match = insert(:match_value_relat)
+      pro = insert(:pro_user)
+      tp = insert(:tp_user)
+      st_pro = insert(:pro_sale_tax, price_sale_tax_count: 0, user: pro)
+      st_tp = insert(:tp_sale_tax, sale_tax_count: 22, user: tp)
+      data = SaleTax.check_match_sale_tax_count(st_tp.id)
+      assert match.match_for_sale_tax_count  == 50
+      assert st_tp.sale_tax_count            == 22
+      assert st_pro.price_sale_tax_count     == 0
+      assert data                            == %{}
+    end
+
+    test "check_match_sale_tax_count if price_sale_tax_count is nil by role Tp" do
+      match = insert(:match_value_relat)
+      pro = insert(:pro_user)
+      tp = insert(:tp_user)
+      st_pro = insert(:pro_sale_tax, price_sale_tax_count: nil, user: pro)
+      st_tp = insert(:tp_sale_tax, sale_tax_count: 22, user: tp)
+      data = SaleTax.check_match_sale_tax_count(st_tp.id)
+      assert match.match_for_sale_tax_count  == 50
+      assert st_tp.sale_tax_count            == 22
+      assert st_pro.price_sale_tax_count     == nil
+      assert data                            == %{}
+    end
+
+    test "check_match_sale_tax_count if sale_tax_count is 0 by role Tp" do
+      match = insert(:match_value_relat)
+      pro = insert(:pro_user)
+      tp = insert(:tp_user)
+      st_pro = insert(:pro_sale_tax, price_sale_tax_count: 33, user: pro)
+      st_tp = insert(:tp_sale_tax, sale_tax_count: 0, user: tp)
+      data = SaleTax.check_match_sale_tax_count(st_tp.id)
+      assert match.match_for_sale_tax_count  == 50
+      assert st_tp.sale_tax_count            == 0
+      assert st_pro.price_sale_tax_count     == 33
+      assert data                            == :error
+    end
+
+    test "check_match_sale_tax_count if sale_tax_count is nil by role Tp" do
+      match = insert(:match_value_relat)
+      pro = insert(:pro_user)
+      tp = insert(:tp_user)
+      st_pro = insert(:pro_sale_tax, price_sale_tax_count: 33, user: pro)
+      st_tp = insert(:tp_sale_tax, sale_tax_count: nil, user: tp)
+      data = SaleTax.check_match_sale_tax_count(st_tp.id)
+      assert match.match_for_sale_tax_count  == 50
+      assert st_tp.sale_tax_count            == nil
+      assert st_pro.price_sale_tax_count     == 33
+      assert data                            == :error
+    end
+
+    test "check_match_sale_tax_count by role Pro" do
+      insert(:match_value_relat)
+      user = insert(:pro_user)
+      struct = insert(:pro_sale_tax, user: user)
+      data = SaleTax.check_match_sale_tax_count(struct.id)
+      assert data == %{}
     end
 
     test "check_match_sale_tax_frequency" do
