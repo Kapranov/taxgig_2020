c1ab700483a603fb3d98a499bd0ae549f01f6952
Oleg G.Kapranov <ok@pure-agency.co>
2020-08-17 00:23:23 -1000

    added Stripy.StripeTesting.Helpers

---
+++ apps/stripy/lib/stripy/stripe_testing/helpers.ex
@@ -0,0 +1,35 @@
+defmodule Stripy.StripeTesting.Helpers do
+  @moduledoc """
+  Used to load JSON fitures which simulate Stripe API responses into
+  stripity_stripe structs
+  """
+
+  @fixture_path "./lib/stripy/stripe_testing/fixtures/"
+
+  @doc """
+  Load a stripe response fixture through stripy, into a struct
+  """
+  @spec load_fixture(String.t) :: struct
+  def load_fixture(id) do
+    id
+    |> load_raw_fixture()
+    |> Stripe.Converter.convert_result
+  end
+
+  @spec load_raw_fixture(String.t) :: map
+  def load_raw_fixture(id) do
+    id
+    |> build_file_path
+    |> File.read!
+    |> Jason.decode!
+  end
+
+  @spec build_file_path(String.t) :: String.t
+  defp build_file_path(id), do: id |> append_extension |> join_with_path
+
+  @spec append_extension(String.t) :: String.t
+  defp append_extension(id), do: id <> ".json"
+
+  @spec join_with_path(String.t) :: String.t
+  defp join_with_path(filename), do: @fixture_path <> filename
+end
