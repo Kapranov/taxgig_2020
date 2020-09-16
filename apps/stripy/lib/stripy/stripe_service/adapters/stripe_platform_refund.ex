76fcc7728fdf7019b732f94b96acd9239be1ce7f
esurat20 <esurat20@gmail.com>
2020-09-17 00:12:24 +0300

    feat: implementing refund and account token services

---
+++ apps/stripy/lib/stripy/stripe_service/adapters/stripe_platform_refund.ex
@@ -0,0 +1,39 @@
+defmodule Stripy.StripeService.Adapters.StripePlatformRefundAdapter do
+  @moduledoc """
+  Transfer model from Stripe.Refund to Application schema model
+  """
+
+  import Stripy.MapUtils, only: [keys_to_string: 1, rename: 3]
+
+  @stripe_attributes [
+    :amount,
+    :balance_transaction,
+    :created,
+    :currency,
+    :id,
+    :status,
+    :charge
+  ]
+
+  @non_stripe_attributes ["user_id"]
+
+  @spec to_params(Stripe.Refund.t(), map) :: {:ok, map}
+  def to_params(%Stripe.Refund{} = stripe_refund, %{} = attributes) do
+    result =
+      stripe_refund
+      |> Map.take(@stripe_attributes)
+      |> rename(:id, :id_from_stripe)
+      |> rename(:charge, :id_from_charge)
+      |> keys_to_string
+      |> add_non_stripe_attributes(attributes)
+
+    {:ok, result}
+  end
+
+  @spec add_non_stripe_attributes(map, map) :: map
+  defp add_non_stripe_attributes(%{} = params, %{} = attributes) do
+    attributes
+    |> Map.take(@non_stripe_attributes)
+    |> Map.merge(params)
+  end
+end
