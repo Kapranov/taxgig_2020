96e6f6725c2877925d2e0d597695f5e2d3ae2634
Oleg G.Kapranov <ok@pure-agency.co>
2020-09-16 22:28:58 -1000

    updated features Stripe.AccountToken by Denis

---
+++ apps/stripy/lib/stripy/stripe_service/adapters/stripe_platform_account_token.ex
@@ -0,0 +1,35 @@
+defmodule Stripy.StripeService.Adapters.StripePlatformAccountTokenAdapter do
+  @moduledoc """
+  Transfer model from Stripe.Token to Application schema model
+  """
+
+  import Stripy.MapUtils, only: [keys_to_string: 1, rename: 3]
+
+  @stripe_attributes [
+    :client_ip,
+    :created,
+    :id,
+    :used
+  ]
+
+  @non_stripe_attributes ["user_id"]
+
+  @spec to_params(Stripe.Token.t(), map) :: {:ok, map}
+  def to_params(%Stripe.Token{} = stripe_account, %{} = attributes) do
+    result =
+      stripe_account
+      |> Map.take(@stripe_attributes)
+      |> rename(:id, :id_from_stripe)
+      |> keys_to_string
+      |> add_non_stripe_attributes(attributes)
+
+    {:ok, result}
+  end
+
+  @spec add_non_stripe_attributes(map, map) :: map
+  defp add_non_stripe_attributes(%{} = params, %{} = attributes) do
+   attributes
+   |> Map.take(@non_stripe_attributes)
+   |> Map.merge(params)
+  end
+end
---
+++ apps/stripy/lib/stripy/stripe_service/stripe_platform_account_token_service.ex
@@ -0,0 +1,72 @@
+defmodule Stripy.StripeService.StripePlatformAccountTokenService do
+  @moduledoc """
+  Used to perform actions on StripeAccountToken records, while propagating to
+  and from associated Stripe.StripeAccountToken records
+  """
+
+  alias Stripy.{
+    Payments,
+    Payments.StripeAccountToken,
+    StripeService.Adapters.StripePlatformAccountTokenAdapter
+  }
+
+  @api Application.get_env(:stripy, :stripe)
+
+  @doc """
+  Creates a new `Stripe.Token` record on Stripe API, as well as an associated local
+  `StripeAccountToken` record
+
+  ## Example
+
+      iex> user_id = "9yk8z0djhUG2r9LMK8"
+      iex> user_attrs = %{"user_id" => user_id}
+      iex> account_attrs = %{
+        account: %{
+          business_type: "individual",
+          individual: %{
+            first_name: "Vlad",
+            last_name: "Puryshev",
+            maiden_name: "Jr",
+            email: "vk@taxgig.com",
+            phone: "999-999-9999",
+            address: %{
+              city: "New York",
+              country: "us",
+              line1: "95 Wall St",
+              postal_code: 10005,
+              state: "NY"
+            },
+            dob: %{
+              day: 15,
+              month: 7,
+              year: 1989
+            },
+            ssn_last_4: "0000"
+          },
+          tos_shown_and_accepted: true
+        }
+      }
+      iex> {:ok, created_token} = Stripe.Token.create(%{account: account_attrs})
+      iex> {:ok, result} = StripePlatformAccountTokenAdapter.to_params(created_token, user_attrs)
+
+  """
+  @spec create(map, map) ::
+          {:ok, StripeAccountToken.t()}
+          | {:error, Ecto.Changeset.t()}
+          | {:error, Stripe.Error.t()}
+          | {:error, :platform_not_ready}
+          | {:error, :not_found}
+  def create(account_attrs, user_attrs) do
+    with {:ok, %Stripe.Token{} = account} = @api.Token.create(%{account: account_attrs}),
+         {:ok, params} <- StripePlatformAccountTokenAdapter.to_params(account, user_attrs)
+    do
+      case Payments.create_stripe_account_token(params) do
+        {:error, error} -> {:error, error}
+        {:ok, data} -> {:ok, data}
+      end
+    else
+      nil -> {:error, :not_found}
+      failure -> failure
+    end
+  end
+end
