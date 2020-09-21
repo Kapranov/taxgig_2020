ab552d91b75283737364076e4ac070936d07e2c3
Oleg G.Kapranov <ok@pure-agency.co>
2020-09-20 22:48:13 -1000

    updated features Stripe.Account by Denis

---
+++ apps/stripy/lib/stripy/stripe_service/adapters/stripe_platform_account.ex
@@ -0,0 +1,44 @@
+defmodule Stripy.StripeService.Adapters.StripePlatformAccountAdapter do
+  @moduledoc """
+  Transfer model from Stripe.Account to Application schema model
+  """
+
+  import Stripy.MapUtils, only: [keys_to_string: 1, rename: 3]
+
+  @stripe_attributes [
+    :business_url,
+    :capabilities,
+    :charges_enabled,
+    :country,
+    :created,
+    :default_currency,
+    :details_submitted,
+    :email,
+    :id,
+    :payout_schedule,
+    :payouts_enabled,
+    :tos_acceptance,
+    :type
+  ]
+
+  @non_stripe_attributes ["user_id"]
+
+  @spec to_params(Stripe.Account.t(), map) :: {:ok, map}
+  def to_params(%Stripe.Account{} = stripe_account, %{} = attributes) do
+    result =
+      stripe_account
+      |> Map.take(@stripe_attributes)
+      |> rename(:id, :id_from_stripe)
+      |> keys_to_string
+      |> add_non_stripe_attributes(attributes)
+
+    {:ok, result}
+  end
+
+  @spec add_non_stripe_attributes(map, map) :: map
+  defp add_non_stripe_attributes(%{} = params, %{} = attributes) do
+    attributes
+    |> Map.take(@non_stripe_attributes)
+    |> Map.merge(params)
+  end
+end
---
+++ apps/stripy/lib/stripy/stripe_service/stripe_platform_account_service.ex
@@ -0,0 +1,81 @@
+defmodule Stripy.StripeService.StripePlatformAccountService do
+  @moduledoc """
+  Used to perform actions on StripeAccount records, while propagating to
+  and from associated Stripe.StripeAccount records
+  """
+
+  alias Stripy.{
+    Payments,
+    Payments.StripeAccount,
+    Queries,
+    Repo,
+    StripeService.Adapters.StripePlatformAccountAdapter
+  }
+
+  @api Application.get_env(:stripy, :stripe)
+
+  @doc """
+  Creates a new `Stripe.Account` record on Stripe API, as well as an associated local
+  `StripeAccount` record
+
+  ## Example
+
+      iex> user_id = "9yk8z0djhUG2r9LMK8"
+      iex> user_attrs = %{"user_id" => user_id}
+      iex> account_attrs = %{
+        account_token: "ct_1HPsraLhtqtNnMebPsawyFas",
+        business_profile: %{
+          mcc: 8931,
+          url: "https://taxgig.com"
+        },
+        capabilities: %{
+          card_payments: %{
+            requested: true
+          },
+          transfers: %{
+            requested: true
+          }
+        },
+        settings: %{
+          payouts: %{
+            schedule: %{
+              interval: "manual"
+            }
+          }
+        }
+      }
+      iex> {:ok, account} = Stripe.Account.create(account_attrs)
+      iex> {:ok, result} = StripePlatformAccountAdapter.to_params(account, user_attrs)
+
+  """
+  @spec create(map, map) ::
+          {:ok, StripeAccount.t()}
+          | {:error, Ecto.Changeset.t()}
+          | {:error, Stripe.Error.t()}
+          | {:error, :platform_not_ready}
+          | {:error, :not_found}
+  def create(account_attrs, user_attrs) do
+    querty =
+      try do
+        Queries.by_count(StripeAccount, :user_id, user_attrs["user_id"])
+      rescue
+        ArgumentError -> :error
+      end
+
+    with {:ok, %Stripe.Account{} = account} = @api.Account.create(account_attrs),
+         {:ok, params} <- StripePlatformAccountAdapter.to_params(account, user_attrs)
+    do
+      case Repo.aggregate(querty, :count, :id) < 1 do
+        false -> {:error, %Ecto.Changeset{}}
+        true ->
+          case Payments.create_stripe_account(params) do
+            {:error, error} -> {:error, error}
+            {:ok, data} -> {:ok, data}
+          end
+      end
+    else
+      nil -> {:error, :not_found}
+      failure -> failure
+    end
+  end
+end
