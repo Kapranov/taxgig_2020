972f5f14d2c1288dc65f9a0493d7842f06d44b4e
Oleg G.Kapranov <ok@pure-agency.co>
2020-11-16 06:41:25 -1000

    added StripePlatformAccountTypes

--- apps/server/lib/server_web/graphql/resolvers/stripe_service/stripe_platform_account_resolver.ex
+++ apps/server/lib/server_web/graphql/resolvers/stripe_service/stripe_platform_account_resolver.ex
@@ -2,4 +2,15 @@ defmodule ServerWeb.GraphQL.Resolvers.StripeService.StripePlatformAccountResolve
   @moduledoc """
   The StripeAccount GraphQL resolvers.
   """
+
+  @type t :: map
+  @type reason :: any
+  @type success_tuple :: {:ok, t}
+  @type success_list :: {:ok, [t]}
+  @type error_tuple :: {:error, reason}
+  @type result :: success_tuple | error_tuple
+
+  @spec delete(any, %{id_from_stripe: bitstring}, Absinthe.Resolution.t()) :: result()
+  def delete(_parent, %{id_from_stripe: _id_from_stripe}, _info) do
+  end
 end
--- apps/server/lib/server_web/graphql/schemas/stripe_service/stripe_platform_account_types.ex
+++ apps/server/lib/server_web/graphql/schemas/stripe_service/stripe_platform_account_types.ex
@@ -4,4 +4,37 @@ defmodule ServerWeb.GraphQL.Schemas.StripeService.StripePlatformAccountTypes do
   """
 
   use Absinthe.Schema.Notation
+
+  import Absinthe.Resolution.Helpers, only: [dataloader: 1]
+
+  alias ServerWeb.GraphQL.{
+    Data,
+    Resolvers.StripeService.StripePlatformAccountResolver
+  }
+
+  @desc "The StripePlatformAccount"
+  object :stripe_platform_account do
+    field :id, non_null(:string)
+    field :id_from_stripe, non_null(:string)
+    field :business_url, non_null(:string)
+    field :capabilities, list_of(:string)
+    field :charges_enabled, non_null(:boolean)
+    field :country, non_null(:string)
+    field :created, non_null(:integer)
+    field :default_currency, non_null(:string)
+    field :details_submitted, non_null(:boolean)
+    field :email, non_null(:string)
+    field :payouts_enabled, non_null(:boolean)
+    field :tos_acceptance, list_of(:string)
+    field :type, non_null(:string)
+    field :users, :user, resolve: dataloader(Data)
+  end
+
+  object :stripe_platform_account_mutations do
+    @desc "Delete a specific the StripePlatformAccount"
+    field :delete_stripe_platform_account, :stripe_platform_account do
+      arg :id_from_stripe, non_null(:string)
+      resolve &StripePlatformAccountResolver.delete/3
+    end
+  end
 end
