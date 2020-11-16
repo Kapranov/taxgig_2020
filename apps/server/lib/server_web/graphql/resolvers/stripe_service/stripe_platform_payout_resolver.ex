f5567c7cebbcc19b31a68e14fccada79c453ca22
Oleg G.Kapranov <ok@pure-agency.co>
2020-11-16 08:44:39 -1000

    added StripePlatformPayoutTypes

--- apps/server/lib/server_web/graphql/resolvers/stripe_service/stripe_platform_payout_resolver.ex
+++ apps/server/lib/server_web/graphql/resolvers/stripe_service/stripe_platform_payout_resolver.ex
@@ -2,4 +2,15 @@ defmodule ServerWeb.GraphQL.Resolvers.StripeService.StripePlatformPayoutResolver
   @moduledoc """
   The StripePayout GraphQL resolvers.
   """
+
+  @type t :: map
+  @type reason :: any
+  @type success_tuple :: {:ok, t}
+  @type success_list :: {:ok, [t]}
+  @type error_tuple :: {:error, reason}
+  @type result :: success_tuple | error_tuple
+
+  @spec create(any, %{atom => any}, Absinthe.Resolution.t()) :: result()
+  def create(_parent, _args, _info) do
+  end
 end
--- apps/server/lib/server_web/graphql/schemas/stripe_service/stripe_platform_payout_types.ex
+++ apps/server/lib/server_web/graphql/schemas/stripe_service/stripe_platform_payout_types.ex
@@ -4,4 +4,44 @@ defmodule ServerWeb.GraphQL.Schemas.StripeService.StripePlatformPayoutTypes do
   """
 
   use Absinthe.Schema.Notation
+
+  alias ServerWeb.GraphQL.Resolvers.StripeService.StripePlatformPayoutResolver
+
+  @desc "The StripePayout"
+  object :stripe_platform_payout do
+    field :id, non_null(:string)
+    field :object, non_null(:string)
+    field :amount, non_null(:integer)
+    field :arrival_date, non_null(:integer)
+    field :automatic, non_null(:boolean)
+    field :balance_transaction, non_null(:string)
+    field :created, non_null(:integer)
+    field :currency, non_null(:string)
+    field :description, non_null(:string)
+    field :destination, non_null(:string)
+    field :failure_balance_transaction, :string
+    field :failure_code, :string
+    field :failure_message, :string
+    field :livemode, non_null(:boolean)
+    field :metadata, list_of(:string)
+    field :method, non_null(:string)
+    field :original_payout, :string
+    field :reversed_by, :string
+    field :source_type, non_null(:string)
+    field :statement_descriptor, :string
+    field :status, non_null(:string)
+    field :type, non_null(:string)
+  end
+
+  object :stripe_platform_payout_mutations do
+    @desc "Create the StripePlatformPayout"
+    field :create_stripe_platform_payout, :stripe_platform_payout_mutations, description: "Create a new stripe platform payout" do
+      arg :amount, non_null(:integer)
+      arg :currency, non_null(:string)
+      arg :destination, non_null(:string)
+      arg :type, non_null(:string)
+      arg :user_id, non_null(:string)
+      resolve &StripePlatformPayoutResolver.create/3
+    end
+  end
 end
