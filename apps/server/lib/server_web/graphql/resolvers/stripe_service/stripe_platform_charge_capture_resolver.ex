9ac561df3485c9bd2ccfd5c0c8fa407960797d1e
Oleg G.Kapranov <ok@pure-agency.co>
2020-11-16 02:13:47 -1000

    added StripePlatformChargeCaptureTypes

--- apps/server/lib/server_web/graphql/resolvers/stripe_service/stripe_platform_charge_capture_resolver.ex
+++ apps/server/lib/server_web/graphql/resolvers/stripe_service/stripe_platform_charge_capture_resolver.ex
@@ -2,4 +2,15 @@ defmodule ServerWeb.GraphQL.Resolvers.StripeService.StripePlatformChargeCaptureR
   @moduledoc """
   The StripeChargeCapture GraphQL resolvers.
   """
+
+  @type t :: map
+  @type reason :: any
+  @type success_tuple :: {:ok, t}
+  @type success_list :: {:ok, [t]}
+  @type error_tuple :: {:error, reason}
+  @type result :: success_tuple | error_tuple
+
+  @spec update(any, %{id: bitstring, stripe_platform_charge_capture: map()}, Absinthe.Resolution.t()) :: result()
+  def update(_root, %{id: _id, stripe_platform_charge_capture: _params}, _info) do
+  end
 end
--- apps/server/lib/server_web/graphql/schemas/stripe_service/stripe_platform_charge_capture_types.ex
+++ apps/server/lib/server_web/graphql/schemas/stripe_service/stripe_platform_charge_capture_types.ex
@@ -4,4 +4,46 @@ defmodule ServerWeb.GraphQL.Schemas.StripeService.StripePlatformChargeCaptureTyp
   """
 
   use Absinthe.Schema.Notation
+
+  import Absinthe.Resolution.Helpers, only: [dataloader: 1]
+
+  alias ServerWeb.GraphQL.{
+    Data,
+    Resolvers.StripeService.StripePlatformChargeCaptureResolver
+  }
+
+  @desc "The StripeChargeCapture"
+  object :stripe_platform_charge_capture do
+    field :id, non_null(:string)
+    field :id_from_card, non_null(:string)
+    field :id_from_customer, non_null(:string)
+    field :id_from_stripe, non_null(:string)
+    field :amount, non_null(:integer)
+    field :amount_refunded, non_null(:integer)
+    field :captured, non_null(:boolean)
+    field :created, non_null(:integer)
+    field :currency, non_null(:string)
+    field :description, non_null(:string)
+    field :failure_code, non_null(:string)
+    field :failure_message, non_null(:string)
+    field :fraud_details,  list_of(:string)
+    field :outcome,  list_of(:string)
+    field :receipt_url, non_null(:string)
+    field :status, non_null(:string)
+    field :users, :user, resolve: dataloader(Data)
+  end
+
+  @desc "The StripePlatformChargeCapture update via params"
+  input_object :update_stripe_platform_charge_capture_params, description: "update stripe platform charge" do
+    field :amount, non_null(:integer), description: "An amount is integer for StripeCharge"
+  end
+
+  object :stripe_platform_charge_capture_mutations do
+    @desc "Update a specific StripePlatformChargeCapture"
+    field :update_stripe_platform_charge_capture, :stripe_platform_charge_capture do
+      arg :id, non_null(:string)
+      arg :stripe_platform_charge_capture, :update_stripe_platform_charge_capture_params
+      resolve &StripePlatformChargeCaptureResolver.update/3
+    end
+  end
 end
