50bf509effa230f0e6aa4bd9ee17519ee6f63b29
Oleg G.Kapranov <ok@pure-agency.co>
2020-11-16 01:06:35 -1000

    added StripePlatformCardTypes

--- apps/server/lib/server_web/graphql/resolvers/stripe_service/stripe_platform_card_resolver.ex
+++ apps/server/lib/server_web/graphql/resolvers/stripe_service/stripe_platform_card_resolver.ex
@@ -2,4 +2,23 @@ defmodule ServerWeb.GraphQL.Resolvers.StripeService.StripePlatformCardResolver d
   @moduledoc """
   The StripeCardToken GraphQL resolvers.
   """
+
+  @type t :: map
+  @type reason :: any
+  @type success_tuple :: {:ok, t}
+  @type success_list :: {:ok, [t]}
+  @type error_tuple :: {:error, reason}
+  @type result :: success_tuple | error_tuple
+
+  @spec list(any, %{atom => any}, Absinthe.Resolution.t()) :: success_list() | error_tuple()
+  def list(_parent, _args, %{context: %{current_user: _current_user}}) do
+  end
+
+  @spec create(any, %{atom => any}, Absinthe.Resolution.t()) :: result()
+  def create(_parent, _args, _info) do
+  end
+
+  @spec delete(any, %{id: bitstring}, Absinthe.Resolution.t()) :: result()
+  def delete(_parent, %{id: _id, customer: _customer}, _info) do
+  end
 end
--- apps/server/lib/server_web/graphql/schemas/stripe_service/stripe_platform_card_types.ex
+++ apps/server/lib/server_web/graphql/schemas/stripe_service/stripe_platform_card_types.ex
@@ -4,4 +4,56 @@ defmodule ServerWeb.GraphQL.Schemas.StripeService.StripePlatformCardTypes do
   """
 
   use Absinthe.Schema.Notation
+
+  import Absinthe.Resolution.Helpers, only: [dataloader: 1]
+
+  alias ServerWeb.GraphQL.{
+    Data,
+    Resolvers.StripeService.StripePlatformCardResolver
+  }
+
+  @desc "The StripeCardToken"
+  object :stripe_platform_card do
+    field :id, non_null(:string)
+    field :brand, non_null(:string)
+    field :client_ip, non_null(:string)
+    field :created, non_null(:integer)
+    field :cvc_check, non_null(:string)
+    field :exp_month, non_null(:integer)
+    field :exp_year, non_null(:integer)
+    field :funding, non_null(:string)
+    field :id_from_customer, non_null(:string)
+    field :id_from_stripe, non_null(:string)
+    field :last4, non_null(:string)
+    field :name, non_null(:string)
+    field :token, non_null(:string)
+    field :used, non_null(:boolean)
+    field :users, :user, resolve: dataloader(Data)
+  end
+
+  object :stripe_platform_card_queries do
+    @desc "Get all StripePlatformCardTokens"
+    field :all_stripe_platform_cards, list_of(:stripe_platform_card) do
+      resolve(&StripePlatformCardResolver.list/3)
+    end
+  end
+
+  object :stripe_platform_card_mutations do
+    @desc "Create the StripePlatformCardToken"
+    field :create_stripe_platform_card, :stripe_platform_card, description: "Create a new stripe platform card token" do
+      arg :cvc, non_null(:integer)
+      arg :exp_month, non_null(:integer)
+      arg :exp_year, non_null(:integer)
+      arg :name, non_null(:string)
+      arg :number, non_null(:integer)
+      resolve &StripePlatformCardResolver.create/3
+    end
+
+    @desc "Delete a specific the StripePlatformCardToken"
+    field :delete_stripe_platform_card, :stripe_platform_card do
+      arg :id_from_customer, non_null(:string)
+      arg :id_from_stripe, non_null(:string)
+      resolve &StripePlatformCardResolver.delete/3
+    end
+  end
 end
