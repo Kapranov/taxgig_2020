2da054ada7f044e1602c0f268714e68d218cdd78
Oleg G.Kapranov <ok@pure-agency.co>
2020-11-17 00:09:46 -1000

    added StripePlatformCustomerResolver

--- apps/server/lib/server_web/graphql/resolvers/stripe_service/stripe_platform_customer_resolver.ex
+++ apps/server/lib/server_web/graphql/resolvers/stripe_service/stripe_platform_customer_resolver.ex
@@ -3,14 +3,42 @@ defmodule ServerWeb.GraphQL.Resolvers.StripeService.StripePlatformCustomerResolv
   The StripeCustomer GraphQL resolvers.
   """
 
-  @type t :: map
+  alias Stripy.{
+    Payments.StripeCustomer,
+    StripeService.StripePlatformCustomerService
+  }
+
+
+  @type t :: StripeCustomer.t()
   @type reason :: any
   @type success_tuple :: {:ok, t}
   @type success_list :: {:ok, [t]}
-  @type error_tuple :: {:error, reason}
+  @type error_tuple :: {:error, reason} |
+                       {:error, Ecto.Changeset.t} |
+                       {:error, Stripe.Error.t()}
   @type result :: success_tuple | error_tuple
 
   @spec delete(any, %{id_from_stripe: bitstring}, Absinthe.Resolution.t()) :: result()
-  def delete(_parent, %{id_from_stripe: _id_from_stripe}, _info) do
+  def delete(_parent, %{id_from_stripe: id_from_stripe}, %{context: %{current_user: current_user}}) do
+    if is_nil(id_from_stripe) do
+      {:error, [[field: :id_from_stripe, message: "Can't be blank"]]}
+    else
+      try do
+        case !is_nil(current_user) do
+          true ->
+            with {:ok, struct} <- StripePlatformCustomerService.delete(id_from_stripe) do
+              {:ok, struct}
+            else
+              nil -> {:error, :not_found}
+              failure -> failure
+            end
+          false ->
+            {:error, "permission denied"}
+        end
+      rescue
+        Ecto.NoResultsError ->
+          {:error, "The StripeCustomer #{id_from_stripe} not found!"}
+      end
+    end
   end
 end
