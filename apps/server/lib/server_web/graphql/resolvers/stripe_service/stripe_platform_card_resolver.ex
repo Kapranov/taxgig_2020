a8cb40f73e0b9b2b6246b81014f9e5a3618e9be3
Oleg G.Kapranov <ok@pure-agency.co>
2020-12-12 06:58:20 -1000

    fixed StripePlatformCardResolver

--- apps/server/lib/server_web/graphql/resolvers/stripe_service/stripe_platform_card_resolver.ex
+++ apps/server/lib/server_web/graphql/resolvers/stripe_service/stripe_platform_card_resolver.ex
@@ -5,15 +5,20 @@ defmodule ServerWeb.GraphQL.Resolvers.StripeService.StripePlatformCardResolver d
 
   alias Core.{
     Accounts,
-    Accounts.User
+    Accounts.User,
+    Queries
   }
 
   alias Stripy.{
+    Payments,
     Payments.StripeCardToken,
     Payments.StripeCustomer,
-    Repo,
-    StripeService.StripePlatformCardService
+    StripeService.StripePlatformCardService,
+    StripeService.StripePlatformCustomerService
   }
+
+  alias Core.Repo, as: CoreRepo
+  alias Stripy.Repo, as: StripyRepo
 
   @type t :: StripeCardToken.t()
   @type reason :: any
@@ -32,7 +37,7 @@ defmodule ServerWeb.GraphQL.Resolvers.StripeService.StripePlatformCardResolver d
       case Accounts.by_role(current_user.id) do
         true -> {:error, :not_found}
         false ->
-          with customer <- Repo.get_by(StripeCustomer, %{user_id: current_user.id}),
+          with customer <- StripyRepo.get_by(StripeCustomer, %{user_id: current_user.id}),
                {:ok, struct} <- StripePlatformCardService.list_card(%{customer: customer})
           do
             {:ok, struct}
@@ -46,17 +51,66 @@ defmodule ServerWeb.GraphQL.Resolvers.StripeService.StripePlatformCardResolver d
 
   @spec create(any, %{atom => any}, %{context: %{current_user: User.t()}}) :: result()
   def create(_parent, args, %{context: %{current_user: current_user}}) do
-    if is_nil(args[:cvc]) || is_nil(args[:exp_month]) || is_nil(args[:exp_year]) || is_nil(args[:name]) || is_nil(args[:number]) do
+    if is_nil(args[:cvc]) ||
+       is_nil(args[:exp_month]) ||
+       is_nil(args[:exp_year]) ||
+       is_nil(args[:name]) ||
+       is_nil(args[:number])
+    do
       {:error, [[field: :stripe_card_token, message: "Can't be blank"]]}
     else
+      querty =
+        try do
+          Queries.by_value(StripeCardToken, :user_id, current_user.id)
+        rescue
+          ArgumentError -> :error
+        end
+
       case Accounts.by_role(current_user.id) do
-        true -> {:error, :not_found}
+        true ->
+          case CoreRepo.aggregate(querty, :count, :id) < 10 do
+            true ->
+              with {:ok, struct} <- StripePlatformCardService.create_token(args, %{"user_id" => current_user.id}) do
+                {:ok, struct}
+              else
+                nil -> {:error, :not_found}
+                failure -> failure
+              end
+            false -> {:error, %Ecto.Changeset{}}
+          end
         false ->
-          with {:ok, struct} <- StripePlatformCardService.create_token(args, %{"user_id" => current_user.id}) do
-            {:ok, struct}
-          else
-            nil -> {:error, :not_found}
-            failure -> failure
+          case CoreRepo.aggregate(querty, :count, :id) do
+            0 ->
+              with {:ok, card} <- StripePlatformCardService.create_token(args, %{"user_id" => current_user.id}),
+                   user <- CoreRepo.get_by(User, id: current_user.id),
+                   full_name <- Accounts.by_full_name(user.id),
+                   {:ok, customer} <- StripePlatformCustomerService.create(%{email: user.email, name: full_name, phone: user.phone, source: card.token}, %{"user_id" => current_user.id})
+              do
+                {:ok, %StripeCardToken{}} = Payments.update_stripe_card_token(card, %{
+                  id_from_customer: customer.id_from_stripe,
+                  token: "xxxxxxxxx"
+                })
+              else
+                nil -> {:error, :not_found}
+                failure -> failure
+              end
+            n ->
+              case n < 10 do
+                true ->
+                  with {:ok, created_token} <- StripePlatformCardService.create_token(args, %{"user_id" => current_user.id}),
+                       id_from_customer <- StripyRepo.get_by(StripeCustomer, %{user_id: current_user.id}).id_from_stripe,
+                       {:ok, created_card} <- StripePlatformCardService.create_card(%{customer: id_from_customer, source: created_token.token})
+                  do
+                    {:ok, %StripeCardToken{}} = Payments.update_stripe_card_token(created_token, %{
+                      id_from_customer: created_card.customer,
+                      token: "xxxxxxxxx"
+                    })
+                  else
+                    nil -> {:error, :not_found}
+                    failure -> failure
+                  end
+                false -> {:error, %Ecto.Changeset{}}
+              end
           end
       end
     end
@@ -70,7 +124,7 @@ defmodule ServerWeb.GraphQL.Resolvers.StripeService.StripePlatformCardResolver d
       try do
         case !is_nil(current_user) do
           true ->
-            with customer <- Repo.get_by(StripeCustomer, %{user_id: current_user.id}),
+            with customer <- StripyRepo.get_by(StripeCustomer, %{user_id: current_user.id}),
                  {:ok, struct} <- StripePlatformCardService.delete_card(id_from_stripe, %{customer: customer.id})
             do
               {:ok, struct}
