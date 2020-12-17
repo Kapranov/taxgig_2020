02439f0bfb6cba5b548d652303d4f8d988242585
Oleg G.Kapranov <ok@pure-agency.co>
2020-12-17 10:00:19 -1000

    added ReportResolver, ReportTypes

---
+++ apps/server/lib/server_web/graphql/resolvers/talk/report_resolver.ex
@@ -0,0 +1,133 @@
+defmodule ServerWeb.GraphQL.Resolvers.Talk.ReportResolver do
+  @moduledoc """
+  The Report GraphQL resolvers.
+  """
+
+  alias Core.{
+    Accounts.User,
+    Repo,
+    Talk,
+    Talk.Report
+  }
+
+  @type t :: Report.t()
+  @type reason :: any
+  @type ok :: {:ok}
+  @type success_tuple :: {:ok, t}
+  @type success_list :: {:ok, [t]}
+  @type error_tuple :: {:error, reason}
+  @type result :: success_tuple | error_tuple
+
+  @spec list(any, %{atom => any}, %{context: %{current_user: User.t()}}) :: result()
+  def list(_parent, _args, %{context: %{current_user: current_user}}) do
+    if is_nil(current_user) do
+      {:error, [[field: :current_user, message: "Permission denied for user current_user to perform action List"]]}
+    else
+      struct = Talk.list_report()
+      {:ok, struct}
+    end
+  end
+
+  @spec list(any, %{atom => any}, Absinthe.Resolution.t()) :: error_tuple
+  def list(_parent, _args, _resolutions) do
+    {:error, "Unauthenticated"}
+  end
+
+  @spec show(any, %{id: bitstring}, %{context: %{current_user: User.t()}}) :: result()
+  def show(_parent, %{id: id}, %{context: %{current_user: current_user}}) do
+    if is_nil(id) || is_nil(current_user) do
+      {:error, [[field: :id, message: "Can't be blank or Permission denied for current_user to perform action Show"]]}
+    else
+      try do
+        struct = Talk.get_report!(id)
+        {:ok, struct}
+      rescue
+        Ecto.NoResultsError ->
+          {:error, "The Report #{id} not found!"}
+      end
+    end
+  end
+
+  @spec show(any, %{atom => any}, Absinthe.Resolution.t()) :: error_tuple()
+  def show(_parent, _args, _info) do
+    {:error, [[field: :current_user,  message: "Unauthenticated"], [field: :id, message: "Can't be blank"]]}
+  end
+
+  @spec create(any, %{atom => any}, %{context: %{current_user: User.t()}}) :: result()
+  def create(_parent, args, %{context: %{current_user: current_user}}) do
+    if is_nil(current_user) do
+      {:error, [[field: :current_user, message: "Permission denied for current_user to perform action Create"]]}
+    else
+      args
+      |> Talk.create_report()
+      |> case do
+        {:ok, struct} ->
+          {:ok, struct}
+        {:error, changeset} ->
+          {:error, extract_error_msg(changeset)}
+      end
+    end
+  end
+
+  @spec create(any, %{atom => any}, Absinthe.Resolution.t()) :: error_tuple()
+  def create(_parent, _args, _info) do
+    {:error, "Unauthenticated"}
+  end
+
+  @spec update(any, %{id: bitstring, report: map()}, %{context: %{current_user: User.t()}}) :: result()
+  def update(_parent, %{id: id, report: params}, %{context: %{current_user: current_user}}) do
+    if is_nil(id) || is_nil(current_user) do
+      {:error, [[field: :id, message: "Can't be blank or Permission denied for current_user to perform action Update"]]}
+    else
+      try do
+        Repo.get!(Report, id)
+        |> Talk.update_report(params)
+        |> case do
+          {:ok, struct} ->
+            {:ok, struct}
+          {:error, changeset} ->
+            {:error, extract_error_msg(changeset)}
+        end
+      rescue
+        Ecto.NoResultsError ->
+          {:error, "The Report #{id} not found!"}
+      end
+    end
+  end
+
+  @spec update(any, %{atom => any}, Absinthe.Resolution.t()) :: error_tuple()
+  def update(_parent, _args, _info) do
+    {:error, [[field: :current_user,  message: "Unauthenticated"], [field: :id, message: "Can't be blank"], [field: :report, message: "Can't be blank"]]}
+  end
+
+  @spec delete(any, %{id: bitstring}, %{context: %{current_user: User.t()}}) :: result()
+  def delete(_parent, %{id: id}, %{context: %{current_user: current_user}}) do
+    if is_nil(id) || is_nil(current_user) do
+      {:error, [[field: :id, message: "Can't be blank or Permission denied for current_user to perform action Delete"]]}
+    else
+      try do
+        struct = Talk.get_report!(id)
+        Repo.delete(struct)
+      rescue
+        Ecto.NoResultsError ->
+          {:error, "The Report #{id} not found!"}
+      end
+    end
+  end
+
+  @spec delete(any, %{atom => any}, Absinthe.Resolution.t()) :: error_tuple()
+  def delete(_parent, _args, _info) do
+    {:error, [[field: :current_user,  message: "Unauthenticated"], [field: :id, message: "Can't be blank"]]}
+  end
+
+  @spec extract_error_msg(Ecto.Changeset.t()) :: Ecto.Changeset.t()
+  defp extract_error_msg(changeset) do
+    changeset.errors
+    |> Enum.map(fn {field, {error, _details}} ->
+      [
+        field: field,
+        message: String.capitalize(error)
+      ]
+    end)
+  end
+end
---
+++ apps/server/lib/server_web/graphql/schemas/talk/report_types.ex
@@ -0,0 +1,86 @@
+defmodule ServerWeb.GraphQL.Schemas.Talk.ReportTypes do
+  @moduledoc """
+  The Report GraphQL interface.
+  """
+
+  use Absinthe.Schema.Notation
+
+  import Absinthe.Resolution.Helpers, only: [dataloader: 1]
+
+  alias ServerWeb.GraphQL.{
+    Data,
+    Resolvers.Talk.ReportResolver
+  }
+
+  @desc "The report on the site"
+  object :report, description: "Report" do
+    field :id, non_null(:string), description: "unique identifier"
+    field :description, :string
+    field :messages, non_null(list_of(:string))
+    field :other, :boolean
+    field :reasons, list_of(:string)
+    field :user, :user, resolve: dataloader(Data)
+  end
+
+  @desc "The report update via params"
+  input_object :update_report_params, description: "update report" do
+    field :description, :string
+    field :messages, list_of(:string)
+    field :other, :boolean
+    field :reasons, list_of(:string)
+    field :user_id, :string
+  end
+
+  object :report_queries do
+    @desc "Get all reports"
+    field :all_reports, list_of(:report) do
+      resolve(&ReportResolver.list/3)
+    end
+
+    @desc "Get a specific report"
+    field :show_report, :report do
+      arg(:id, non_null(:string))
+      resolve(&ReportResolver.show/3)
+    end
+  end
+
+  object :report_mutations do
+    @desc "Create the report"
+    field :create_report, :report, description: "Create a new report" do
+      arg :description, :string
+      arg :messages, non_null(list_of(:string))
+      arg :other, :boolean
+      arg :reasons, list_of(:string)
+      arg :user_id, non_null(:string)
+      resolve &ReportResolver.create/3
+    end
+
+    @desc "Update a specific report"
+    field :update_report, :report do
+      arg :id, non_null(:string)
+      arg :report, :update_report_params
+      resolve &ReportResolver.update/3
+    end
+
+    @desc "Delete a specific the report"
+    field :delete_report, :report do
+      arg :id, non_null(:string)
+      resolve &ReportResolver.delete/3
+    end
+  end
+
+  object :report_subscriptions do
+    @desc "Create the report via channel"
+    field :report_created, :report do
+      config(fn _, _ ->
+        {:ok, topic: "reports"}
+      end)
+
+      trigger(:create_report,
+        topic: fn _ ->
+          "reports"
+        end
+      )
+    end
+  end
+end
