563b6b24153afafbb77ec775373e1579aa05ba7f
Oleg G.Kapranov <ok@pure-agency.co>
2023-02-03 04:56:26 -1000

    updated reptin import.sh

--- apps/reptin/bin/import.sh
+++ apps/reptin/bin/import.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-HOST="taxgig.com"
+HOST="api.taxgig.com"
 PORT="28015"
 
 rethinkdb-import -c $HOST:$PORT -f $1 --force --format csv --table ptin.ptins --pkey id --fields last_name,first_name,bus_st_code,bus_addr_zip,profession
