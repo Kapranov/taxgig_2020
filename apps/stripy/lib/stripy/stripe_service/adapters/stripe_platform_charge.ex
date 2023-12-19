6cef1ab8509333aaf947d790c50835dbf668906a
Oleg G.Kapranov <ok@pure-agency.co>
2023-12-19 01:47:38 -1000

    updated Stripy.StripeService.Adapters.StripePlatformChargeAdapter.to_params_for_list

--- apps/stripy/lib/stripy/stripe_service/adapters/stripe_platform_charge.ex
+++ apps/stripy/lib/stripy/stripe_service/adapters/stripe_platform_charge.ex
@@ -52,46 +52,85 @@ defmodule Stripy.StripeService.Adapters.StripePlatformChargeAdapter do
 
   @spec to_params_for_list(String.t()) :: {:ok, map}
   def to_params_for_list(stripe_charge) do
-    %{source: source} =
-      stripe_charge
-      |> List.first
-      |> Map.from_struct
-      |> Map.take([:source])
+#    %{source: source} =
+#      stripe_charge
+#      |> List.first
+#      |> Map.from_struct
+#      |> Map.take([:source])
+#
+#    source_data = %{
+#      source: %{
+#        brand: source.brand,
+#        funding: source.funding,
+#        id: source.id,
+#        last4: source.last4,
+#        object: source.object
+#      }
+#    }
+#
+#    %{payment_method_details: payment_method_details} =
+#      stripe_charge
+#      |> List.first
+#      |> Map.from_struct
+#      |> Map.take([:payment_method_details])
+#
+#    payment_method_details_data = %{
+#      payment_method_details: %{
+#        brand: payment_method_details.card.brand,
+#        funding: payment_method_details.card.funding,
+#        last4: payment_method_details.card.last4,
+#        type: payment_method_details.type
+#      }
+#    }
+#
+#    result =
+#      stripe_charge
+#      |> List.first
+#      |> Map.from_struct
+#      |> Map.take(@stripe_list_charges_attributes)
+#      |> Map.merge(source_data)
+#      |> Map.merge(payment_method_details_data)
+#
+#    {:ok, result}
+    Enum.reduce(stripe_charge, [], fn(item, acc) ->
+      %{source: source} =
+        item
+        |> Map.from_struct
+        |> Map.take([:source])
 
-    source_data = %{
-      source: %{
-        brand: source.brand,
-        funding: source.funding,
-        id: source.id,
-        last4: source.last4,
-        object: source.object
+      source_data = %{
+        source: %{
+          brand: source.brand,
+          funding: source.funding,
+          id: source.id,
+          last4: source.last4,
+          object: source.object
+        }
       }
-    }
 
-    %{payment_method_details: payment_method_details} =
-      stripe_charge
-      |> List.first
-      |> Map.from_struct
-      |> Map.take([:payment_method_details])
+      %{payment_method_details: payment_method_details} =
+        item
+        |> Map.from_struct
+        |> Map.take([:payment_method_details])
 
-    payment_method_details_data = %{
-      payment_method_details: %{
-        brand: payment_method_details.card.brand,
-        funding: payment_method_details.card.funding,
-        last4: payment_method_details.card.last4,
-        type: payment_method_details.type
+      payment_method_details_data = %{
+        payment_method_details: %{
+          brand: payment_method_details.card.brand,
+          funding: payment_method_details.card.funding,
+          last4: payment_method_details.card.last4,
+          type: payment_method_details.type
+        }
       }
-    }
 
-    result =
-      stripe_charge
-      |> List.first
-      |> Map.from_struct
-      |> Map.take(@stripe_list_charges_attributes)
-      |> Map.merge(source_data)
-      |> Map.merge(payment_method_details_data)
+      result =
+        item
+        |> Map.from_struct
+        |> Map.take(@stripe_list_charges_attributes)
+        |> Map.merge(source_data)
+        |> Map.merge(payment_method_details_data)
 
-    {:ok, result}
+      [result | acc]
+    end)
   end
 
   @spec add_non_stripe_attributes(map, map) :: map
