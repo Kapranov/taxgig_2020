5e455ec4257b0f174a10049d47c1b8e9a929790c
Oleg G.Kapranov <ok@pure-agency.co>
2024-03-26 21:36:55 -1000

    updated dev.secret.exs

--- config/dev.secret.exs
+++ config/dev.secret.exs
@@ -89,12 +89,22 @@ config :ex_aws,
 config :server, Google,
   client_id: "991262252553-18hlqfkgmkmk9l9o1niuq0ehcqvd097u.apps.googleusercontent.com",
   client_secret: "GYyCMYA1hLMYAJrfBZoJxHa0",
-  redirect_uri: ["https://app.taxgig.com/auth/registration-creating-account?provider=google", "https://app.taxgig.com/auth/login?provider=google", "https://localhost:4200/auth/login?provider=google", "https://localhost:4200/auth/registration-creating-account?provider=google"]
+  redirect_uri: [
+    "https://app.taxgig.com/auth/login?provider=google",
+    "https://app.taxgig.com/auth/registration-creating-account?provider=google",
+    "https://localhost:4200/auth/login?provider=google",
+    "https://localhost:4200/auth/registration-creating-account?provider=google"
+  ]
 
 config :server, Facebook,
   client_id: "693824267691319",
   client_secret: "250e5db3f21f6138c75f551f758a8652",
-  redirect_uri: ["https://app.taxgig.com/auth/registration-creating-account?provider=facebook", "https://app.taxgig.com/auth/login?provider=facebook", "https://localhost:4200/auth/registration-creating-account?provider=facebook", "https://localhost:4200/auth/login?provider=facebook"],
+  redirect_uri: [
+    "https://app.taxgig.com/auth/login?provider=facebook",
+    "https://app.taxgig.com/auth/registration-creating-account?provider=facebook",
+    "https://localhost:4200/auth/login?provider=facebook",
+    "https://localhost:4200/auth/registration-creating-account?provider=facebook"
+  ],
   scope: "email,user_photos"
 
 config :server, Twitter,
@@ -102,9 +112,17 @@ config :server, Twitter,
   access_token_secret: "d4wYVSk7A7VYjo2fD9J5OssEklz11DY2m1SMkfRdlpEwO",
   consumer_key: "EjOshf6LQJqBWAxXL1OgRV7dk",
   consumer_secret: "h24eToLvYW9SAh5zfin68VXroYqfzEVNYFxuJgyZaYZ8m2lqcm",
-  redirect_uri: ["https://localhost:4200/auth/registration-creating-account?provider=twitter", "https://localhost:4200/auth/login?provider=twitter"]
+  redirect_uri: [
+    "https://localhost:4200/auth/login?provider=twitter",
+    "https://localhost:4200/auth/registration-creating-account?provider=twitter"
+  ]
 
 config :server, LinkedIn,
   client_id: "77em5r3mjow7oy",
   client_secret: "gyAt8UOtXXtYH2jY",
-  redirect_uri: ["https://app.taxgig.com/auth/registration-creating-account?provider=linkedin", "https://app.taxgig.com/auth/login?provider=linkedin", "https://localhost:4200/auth/registration-creating-account?provider=linkedin", "https://localhost:4200/auth/login?provider=linkedin"]
+  redirect_uri: [
+    "https://app.taxgig.com/auth/login?provider=linkedin",
+    "https://app.taxgig.com/auth/registration-creating-account?provider=linkedin",
+    "https://localhost:4200/auth/login?provider=linkedin",
+    "https://localhost:4200/auth/registration-creating-account?provider=linkedin"
+  ]
