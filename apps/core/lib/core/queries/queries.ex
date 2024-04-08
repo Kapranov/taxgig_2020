fc42d92f3dadfb9a11d8f49ece6c801cd86a915e
Oleg G.Kapranov <ok@pure-agency.co>
2024-04-08 03:05:09 -1000

    fixed Core.Queries

--- apps/core/lib/core/queries/queries.ex
+++ apps/core/lib/core/queries/queries.ex
@@ -195,7 +195,6 @@ defmodule Core.Queries do
       Repo.all(
         from c in struct_a,
         join: cu in ^struct_b,
-        where: not is_nil(cu.participant_id),
         where: c.user_id == ^user_id and c.user_id == cu.recipient_id,
         where: cu.is_read == false,
         select: count(cu.id)
@@ -228,8 +227,6 @@ defmodule Core.Queries do
       Repo.all(
         from c in struct_a,
         join: cu in ^struct_b,
-        where: not is_nil(c.participant_id),
-        where: not is_nil(cu.participant_id),
         where: c.participant_id == ^participant_id and c.participant_id == cu.recipient_id,
         where: cu.is_read == false,
         select: count(cu.id)
